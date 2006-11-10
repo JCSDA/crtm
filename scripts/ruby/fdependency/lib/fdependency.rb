65f087cc5f417810ac782ad1c6034898a453f2c4
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2006-11-10 02:00:26 +0000

    First (somewhat) working version that uses Funit dependency clas.

--- scripts/ruby/fdependency/lib/fdependency.rb
+++ scripts/ruby/fdependency/lib/fdependency.rb
@@ -1,10 +1,19 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby -w
 
 require 'funit/fortran_deps'
 
-x=Funit::Depend.new
-ARGV.each do |file|
-  puts(x.makefile_dependency_line(file))
+# Redefine the Funit::Depend fortran_files_within method
+class Funit::Depend
+  def fortran_files_within( search_path = %w[ ../lib . ] )
+    source = search_path.map{ |path| Dir[path+"/*.[fF]90"] }
+    source.flatten!.uniq!
+  end
 end
 
-puts("got this far!")
+# Output a dependency list
+searchPath=["."]
+fdepList=Funit::Depend.new(searchPath)
+puts(fdepList.inspect)
+ARGV.each do |file|
+  puts fdepList.makefile_dependency_line(file)
+end
