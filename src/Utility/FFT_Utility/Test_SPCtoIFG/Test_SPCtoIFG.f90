e1b558f4b4455c71a7afe4823fb64a142047e356
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2006-11-29 23:04:33 +0000

    Test code updated to read and write "Binary" format files with endian-ness checking.

--- src/Utility/FFT_Utility/Test_SPCtoIFG/Test_SPCtoIFG.f90
+++ src/Utility/FFT_Utility/Test_SPCtoIFG/Test_SPCtoIFG.f90
@@ -1,13 +1,13 @@
 PROGRAM Test_SPCtoIFG
 
   USE Type_Kinds           , ONLY: fp=>fp_kind
-  USE File_Utility         , ONLY: Get_Lun
+  USE Binary_File_Utility  , ONLY: Open_Binary_File
   USE Message_Handler      , ONLY: SUCCESS
   USE Fundamental_Constants, ONLY: PI
   USE FFT_Spectral_Utility , ONLY: SPCtoIFG
   IMPLICIT NONE
   ! Parameters
-  CHARACTER(*), PARAMETER :: FILENAME='fft_test_spc.bin'
+  CHARACTER(*), PARAMETER :: FILENAME='test_spc.bin'
   ! Variables
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: f
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: rSpc, iSpc
@@ -19,50 +19,55 @@ PROGRAM Test_SPCtoIFG
   INTEGER :: i, j, errStatus, ioStatus, allocStatus
     
   ! Read the spectral data
-  fileId = Get_Lun()
-  OPEN(fileId, FILE  =FILENAME     , &
-               STATUS='OLD'        , & 
-               FORM  ='UNFORMATTED', & 
-               ACCESS='SEQUENTIAL' , & 
-               IOSTAT=ioStatus       ) 
-  IF ( ioStatus /= 0 ) THEN
-    WRITE(*,*) 'Error opening test file. IOSTAT=',ioStatus
+  WRITE(*,'(/5x,"Reading input test spectrum...")' )
+  errStatus = Open_Binary_File( FILENAME, &
+                                fileId    )
+  IF ( errStatus /= SUCCESS ) THEN
+    WRITE(*,*) 'Error opening test spectrum file.'
     STOP
   END IF
-  
-  READ(fileId) n
-  n2 = 2*(n-1)
-
+  READ(fileId) n  ! The number of SPC points
+  n2 = 2*(n-1)    ! The number of IFG points
   ALLOCATE(f(n), rSpc(n), iSpc(n), spc(n), x(n2), ifg(n2), STAT=allocStatus)
   IF ( allocStatus /= 0 ) THEN
     WRITE(*,*) 'Error allocating arrays. STAT=',allocStatus
     STOP
   END IF
-
-  READ(fileId)f
-  READ(fileId)rSpc
-  READ(fileId)iSpc
-  
+  READ(fileId)f     ! Frequency data
+  READ(fileId)rSpc  ! Real part of SPC
+  READ(fileId)iSpc  ! Imaginary part of SPC
   CLOSE(fileId)
 
-  ! Call the routine
+  ! Make the spectrum complex
   spc = CMPLX(rSpc,iSpc,fp)
+
+  ! Call the routine
+  WRITE(*,'(5x,"Calling SPCtoIFG......")' )
   errStatus=SPCtoIFG(f, Spc, x, ifg)
   IF ( errStatus /= SUCCESS ) THEN
     WRITE(*,*) 'Error in SPCtoIFG call'
     STOP
   END IF
 
-  ! Output an ASCII file for viewing results
-  fileId = Get_Lun()
-  OPEN(fileId,FILE='Test_SPCtoIFG.dat',STATUS='UNKNOWN')
-  WRITE(fileId,*) n
-  DO i=1, n
-    WRITE(fileId,*) f(i), rSpc(i), iSpc(i) 
-  END DO
-  DO i=1, n2
-    WRITE(fileId,*) x(i), REAL(ifg(i),fp),AIMAG(ifg(i))
-  END DO
+  ! Output the SPC and IFG to a file
+  WRITE(*,'(5x,"Writing output file......")' )
+  errStatus = Open_Binary_File( 'Test_SPCtoIFG.bin', &
+                                fileId, &
+                                For_Output=1 )
+  IF ( errStatus /= SUCCESS ) THEN
+    WRITE(*,*) 'Error opening output results file.'
+    STOP
+  END IF
+  ! The SPC data
+  WRITE(fileId) n            ! The number of SPC points
+  WRITE(fileId) f            ! Frequency data
+  WRITE(fileId) rSpc         ! Real part of SPC
+  WRITE(fileId) iSpc         ! Imaginary part of SPC
+  ! The IFG data
+  WRITE(fileId) n2           ! The number of IFG points
+  WRITE(fileId) x            ! Optical delay data
+  WRITE(fileId) REAL(ifg,fp) ! Real part of IFG
+  WRITE(fileId) AIMAG(ifg)   ! Imaginary part of IFG
   CLOSE(fileId)
   
 END PROGRAM Test_SPCtoIFG
