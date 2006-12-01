2b6b5251f4a07644dbbb649054d9eba0366e55dc
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2006-12-01 23:43:27 +0000

    Initial checkin.

---
+++ src/Utility/FFT_Utility/Prime_Utility/Test/Test_Prime_Utility.f90
@@ -0,0 +1,36 @@
+PROGRAM Test_Prime_Utility
+  USE Prime_Utility
+  IMPLICIT NONE
+  ! Parameters
+  INTEGER, PARAMETER :: NTEST=500
+  INTEGER, PARAMETER :: NPFTEST=9438
+  ! Variables
+  INTEGER :: i, j
+  INTEGER :: prod
+  TYPE(Prime_type) :: Prime
+
+  WRITE(*,'(/5x,"Primes in range 1..",i0," derived via IsPrime:")') NTEST
+  j=0
+  DO i = 1, NTEST
+    IF (IsPrime(i)) THEN
+      j=j+1
+      IF(MOD(j-1,10)==0 .AND. j/=1) WRITE(*,*)
+      WRITE(*,'(1x,i5)',ADVANCE='NO') i
+    END IF
+  END DO
+
+  Prime=Primes(NTEST)
+  WRITE(*,'(//5x,"Primes in range 1..",i0," derived via Primes:")') Prime%number
+  WRITE(*,'(10(1x,i5))') Prime%n
+
+  Prime=PrimeFactor(NPFTEST)
+  WRITE(*,'(//5x,"Prime factors for ",i0,":")') Prime%number
+  prod=1
+  DO i=1,Prime%nPrimes
+    prod=prod*(Prime%n(i)**Prime%nExp(i))
+    WRITE(*,'(i0,"^",i0)',ADVANCE='NO') Prime%n(i), Prime%nExp(i)
+    IF(i < Prime%nPrimes) WRITE(*,'(" x ")',ADVANCE='NO')
+  END DO
+  WRITE(*,'(" = ",i0)')prod
+  
+END PROGRAM Test_Prime_Utility
