6e44adb579986258b3d39823a6adae7664559d72
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2006-12-01 23:45:58 +0000

    Test code updated to read and write "Binary" format files with endian-ness checking.

--- src/Utility/FFT_Utility/Test_Fourier_Interpolate/Test_Fourier_Interpolate.f90
+++ src/Utility/FFT_Utility/Test_Fourier_Interpolate/Test_Fourier_Interpolate.f90
@@ -1,13 +1,13 @@
 PROGRAM Test_Fourier_Interpolate
 
   USE Type_Kinds           , ONLY: fp=>fp_kind
-  USE File_Utility         , ONLY: Get_Lun
+  USE Binary_File_Utility  , ONLY: Open_Binary_File
   USE Message_Handler      , ONLY: SUCCESS
   USE Fundamental_Constants, ONLY: PI
   USE FFT_Spectral_Utility , ONLY: Fourier_Interpolate
   IMPLICIT NONE
   ! Parameters
-  CHARACTER(*), PARAMETER :: FILENAME='fint_test_spc.bin'
+  CHARACTER(*), PARAMETER :: FILENAME='test_spc.bin'
   ! Variables
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: fIn, spcIn
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: fOut, spcOut
@@ -16,32 +16,26 @@ PROGRAM Test_Fourier_Interpolate
   INTEGER :: i, j, errStatus, ioStatus, allocStatus
     
   ! Read the spectral data
-  fileId = Get_Lun()
-  OPEN(fileId, FILE  =FILENAME     , &
-               STATUS='OLD'        , & 
-               FORM  ='UNFORMATTED', & 
-               ACCESS='SEQUENTIAL' , & 
-               IOSTAT=ioStatus       ) 
-  IF ( ioStatus /= 0 ) THEN
-    WRITE(*,*) 'Error opening test file. IOSTAT=',ioStatus
+  WRITE(*,'(/5x,"Reading input test spectrum...")' )
+  errStatus = Open_Binary_File( FILENAME, &
+                                fileId    )
+  IF ( errStatus /= SUCCESS ) THEN
+    WRITE(*,*) 'Error opening test spectrum file.'
     STOP
   END IF
-  
-  READ(fileId) n
-  n2 = 2**15 + 1
-
+  READ(fileId) n  ! The number of SPC points
+  n2 = 2**15 + 1  ! The number of zerofilled IFG points
   ALLOCATE(fIn(n), spcIn(n), fOut(n2), spcOut(n2), STAT=allocStatus)
   IF ( allocStatus /= 0 ) THEN
     WRITE(*,*) 'Error allocating arrays. STAT=',allocStatus
     STOP
   END IF
-
-  READ(fileId)fIn
-  READ(fileId)spcIn ! Only read the real part of the spectrum
-  
+  READ(fileId)fIn   ! Frequency data
+  READ(fileId)spcIn ! Only read the real part of SPC 
   CLOSE(fileId)
 
   ! Call the routine
+  WRITE(*,'(5x,"Calling Fouier_Interpolate...")' )
   errStatus=Fourier_Interpolate(fIn, spcIn, fOut, spcOut, FilterWidth=10.0_fp)
   IF ( errStatus /= SUCCESS ) THEN
     WRITE(*,*) 'Error in Fourier_Interpolate call'
@@ -49,15 +43,22 @@ PROGRAM Test_Fourier_Interpolate
   END IF
 
   ! Output an ASCII file for viewing results
-  fileId = Get_Lun()
-  OPEN(fileId,FILE='Test_Fourier_Interpolate.dat',STATUS='UNKNOWN')
-  WRITE(fileId,*) n, n2
-  DO i=1, n
-    WRITE(fileId,*) fIn(i), spcIn(i)
-  END DO
-  DO i=1, n2
-    WRITE(fileId,*) fOut(i), spcOut(i)
-  END DO
+  WRITE(*,'(5x,"Writing output file......")' )
+  errStatus = Open_Binary_File( 'Test_Fourier_Interpolate.bin', &
+                                fileId, &
+                                For_Output=1 )
+  IF ( errStatus /= SUCCESS ) THEN
+    WRITE(*,*) 'Error opening output results file.'
+    STOP
+  END IF
+  ! The original SPC data (REAL ONLY)
+  WRITE(fileId) n            ! The number of SPC points
+  WRITE(fileId) fIn          ! Frequency data
+  WRITE(fileId) spcIn        ! Real part of SPC
+  ! The interpolated SPC data (REAL ONLY)
+  WRITE(fileId) n2           ! The number of SPC points
+  WRITE(fileId) fOut         ! Frequency data
+  WRITE(fileId) spcOut       ! Real part of SPC
   CLOSE(fileId)
   
 END PROGRAM Test_Fourier_Interpolate
