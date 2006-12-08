beedf3f6dd05dcb17e3e0c9b2804a4619cf5d432
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2006-12-08 00:48:52 +0000

    - Added option for user to select input datafile. - Multiple filter widths are now processed to determine its impact on   the interpolation.

--- src/Utility/FFT_Utility/Test_Fourier_Interpolate/Test_Fourier_Interpolate.f90
+++ src/Utility/FFT_Utility/Test_Fourier_Interpolate/Test_Fourier_Interpolate.f90
@@ -7,24 +7,36 @@ PROGRAM Test_Fourier_Interpolate
   USE FFT_Spectral_Utility , ONLY: Fourier_Interpolate
   IMPLICIT NONE
   ! Parameters
-  CHARACTER(*), PARAMETER :: FILENAME='test_spc.bin'
+  CHARACTER(*), PARAMETER :: INFILE(2)=(/'test_spc.bin   ','test_boxcar.bin'/)
+  INTEGER,      PARAMETER :: NFILTERS=4
+  REAL(fp),     PARAMETER :: FILTERWIDTH(NFILTERS) = (/10.0_fp,20.0_fp,50.0_fp,100.0_fp/)
   ! Variables
+  CHARACTER(256) :: outFile
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: fIn, spcIn
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: fOut, spcOut
-  INTEGER :: fileId
-  INTEGER :: n, n2
-  INTEGER :: i, j, errStatus, ioStatus, allocStatus
-    
+  INTEGER  :: fileId
+  INTEGER  :: i, j, n, n2
+  INTEGER  :: errStatus, allocStatus
+  CHARACTER(5) :: cFilterWidth
+  
+  ! Select spectral data
+  WRITE(*,'(/5x,"Select spectral data:")')
+  DO i=1,2
+    WRITE(*,'(10x,i1,") " ,a)') i, INFILE(i)
+  END DO
+  WRITE(*,'(5x,"Enter choice: ")',ADVANCE='NO')
+  READ(*,*) i
+
   ! Read the spectral data
-  WRITE(*,'(/5x,"Reading input test spectrum...")' )
-  errStatus = Open_Binary_File( FILENAME, &
-                                fileId    )
+  WRITE(*,'(/5x,"Reading input test spectrum ",a,"...")' ) TRIM(INFILE(i))
+  errStatus = Open_Binary_File( INFILE(i), &
+                                fileId     )
   IF ( errStatus /= SUCCESS ) THEN
     WRITE(*,*) 'Error opening test spectrum file.'
     STOP
   END IF
   READ(fileId) n  ! The number of SPC points
-  n2 = 2**15 + 1  ! The number of zerofilled IFG points
+  n2 = 2**14 + 1  ! The number of zerofilled IFG points
   ALLOCATE(fIn(n), spcIn(n), fOut(n2), spcOut(n2), STAT=allocStatus)
   IF ( allocStatus /= 0 ) THEN
     WRITE(*,*) 'Error allocating arrays. STAT=',allocStatus
@@ -34,31 +46,40 @@ PROGRAM Test_Fourier_Interpolate
   READ(fileId)spcIn ! Only read the real part of SPC 
   CLOSE(fileId)
 
-  ! Call the routine
-  WRITE(*,'(5x,"Calling Fouier_Interpolate...")' )
-  errStatus=Fourier_Interpolate(fIn, spcIn, fOut, spcOut, FilterWidth=10.0_fp)
-  IF ( errStatus /= SUCCESS ) THEN
-    WRITE(*,*) 'Error in Fourier_Interpolate call'
-    STOP
-  END IF
+  ! Loop over filter widths
+  DO j=1,NFILTERS
+  
+    WRITE(cFilterWidth,'(f5.1)') FILTERWIDTH(j)
+    cFilterWidth=ADJUSTL(cFilterWidth)
+    
+    ! Call the routine
+    WRITE(*,'(/5x,"Calling Fourier_Interpolate with filter width of ",a,"cm-1...")' ) TRIM(cFilterWidth)
+    errStatus=Fourier_Interpolate(fIn, spcIn, fOut, spcOut, &
+                                  FilterWidth=FILTERWIDTH(j))
+    IF ( errStatus /= SUCCESS ) THEN
+      WRITE(*,*) 'Error in Fourier_Interpolate call'
+      STOP
+    END IF
 
-  ! Output an ASCII file for viewing results
-  WRITE(*,'(5x,"Writing output file......")' )
-  errStatus = Open_Binary_File( 'Test_Fourier_Interpolate.bin', &
-                                fileId, &
-                                For_Output=1 )
-  IF ( errStatus /= SUCCESS ) THEN
-    WRITE(*,*) 'Error opening output results file.'
-    STOP
-  END IF
-  ! The original SPC data (REAL ONLY)
-  WRITE(fileId) n            ! The number of SPC points
-  WRITE(fileId) fIn          ! Frequency data
-  WRITE(fileId) spcIn        ! Real part of SPC
-  ! The interpolated SPC data (REAL ONLY)
-  WRITE(fileId) n2           ! The number of SPC points
-  WRITE(fileId) fOut         ! Frequency data
-  WRITE(fileId) spcOut       ! Real part of SPC
-  CLOSE(fileId)
+    ! Output an ASCII file for viewing results
+    WRITE(outFile,'("Test_Fourier_Interpolate.fw-",a,".",a)') TRIM(cFilterWidth), TRIM(INFILE(i))
+    WRITE(*,'(5x,"Writing output file ",a,"...")' ) TRIM(outFile)
+    errStatus = Open_Binary_File( outFile, &
+                                  fileId, &
+                                  For_Output=1 )
+    IF ( errStatus /= SUCCESS ) THEN
+      WRITE(*,*) 'Error opening output results file.'
+      STOP
+    END IF
+    ! The original SPC data (REAL ONLY)
+    WRITE(fileId) n            ! The number of SPC points
+    WRITE(fileId) fIn          ! Frequency data
+    WRITE(fileId) spcIn        ! Real part of SPC
+    ! The interpolated SPC data (REAL ONLY)
+    WRITE(fileId) n2           ! The number of SPC points
+    WRITE(fileId) fOut         ! Frequency data
+    WRITE(fileId) spcOut       ! Real part of SPC
+    CLOSE(fileId)
+  END DO
   
 END PROGRAM Test_Fourier_Interpolate
