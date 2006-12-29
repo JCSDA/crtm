d4d56040fe2626b1af718c3893737ad9751ca8e7
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2006-12-29 16:06:12 +0000

    Added index finding functions for both regularly and randomly spaced vectors.
    Former-commit-id: daf01e06cbd8ef0d417b443b4074561321fd235c

--- src/CRTM_Utility/Interp_ND.f90
+++ src/CRTM_Utility/Interp_ND.f90
@@ -4,6 +4,13 @@ MODULE Interp_ND
   IMPLICIT NONE
   PRIVATE
   PUBLIC :: Interp_2D
+  PUBLIC :: findIdx
+  
+  INTERFACE findIdx
+    MODULE PROCEDURE findRegularIdx
+    MODULE PROCEDURE findRandomIdx
+  END INTERFACE findIdx
+  
   CHARACTER(*), PARAMETER :: MODULE_RCS_ID=&
   '$Id: $'
   REAL(fp), PARAMETER :: ZERO=0.0_fp
@@ -21,5 +28,29 @@ CONTAINS
            (ONE-d1)*   d2   *y(1,2) + &   
               d1   *   d2   *y(2,2)
   END FUNCTION Interp_2D 
+
+  ! Find lower index for regular spacing
+  FUNCTION findRegularIdx(x1, dx, xInt) RESULT(idx)
+    REAL(fp), INTENT(IN) :: x1, dx, xInt
+    INTEGER :: idx
+    idx = FLOOR((xInt-x1)/dx) + 1
+  END FUNCTION findRegularIdx
+  
+  ! Find lower index for random spacing.
+  ! Assumption is that x(1) <= xInt <= x(n)
+  ! (despite the MIN/MAX test)
+  FUNCTION findRandomIdx(x, xInt) RESULT(idx)
+    REAL(fp), INTENT(IN) :: x(:)
+    REAL(fp), INTENT(IN) :: xInt
+    INTEGER :: idx
+    INTEGER :: k, n
+    n = SIZE(x)
+    DO k=1,n
+      IF (xInt <= x(k) ) EXIT
+    END DO
+    idx = MIN(MAX(1,k-1),n-1)
+  END FUNCTION findRandomIdx
+  
+ 
 
 END MODULE Interp_ND
