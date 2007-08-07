54c8b9b9779c1f1a3319b84894e2e5a68c1240af
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2007-08-07 21:35:27 +0000

    src:TauProd/Infrared/LBL_Utility/LBL2NC subdirectory.   * LBL2NC.f90: Added panel parameters to ONLY list for USE LBLRTM_Parameters statement.     Added code to prompt user to enter the calculation direction and the number of panels     in the LBLRTM-format file. Removed code to detect calculation direction directly from     the LBLRTM-format file. Read loop exit shifted to the end of the read loop.
    Former-commit-id: c75e041ee892bdadad7eaffc8957b3b1b2fd9c62

--- src/TauProd/Infrared/LBLRTM_Utility/LBL2NC/LBL2NC.f90
+++ src/TauProd/Infrared/LBLRTM_Utility/LBL2NC/LBL2NC.f90
@@ -25,7 +25,11 @@ PROGRAM LBL2NC
   USE File_Utility     , ONLY: File_Exists
   USE Message_Handler  , ONLY: SUCCESS, FAILURE, INFORMATION, &
                                Display_Message, Program_Message
-  USE LBLRTM_Parameters, ONLY: LBLRTM_SINGLE_PANEL_TYPE, &
+  USE LBLRTM_Parameters, ONLY: LBLRTM_N_PANEL_TYPES, &
+                               LBLRTM_N_PANELS, &
+                               LBLRTM_PANEL_TYPE, &
+                               LBLRTM_PANEL_TYPE_NAME, &
+                               LBLRTM_SINGLE_PANEL_TYPE, &
                                LBLRTM_FILE_PTR_EOF
   USE LBLRTM_Utility   , ONLY: Open_LBLRTM, Compute_n_Points
   USE LBLRTM_Fhdr_IO   , ONLY: LBLRTM_Fhdr_type, &
@@ -45,10 +49,12 @@ PROGRAM LBL2NC
   CHARACTER(*), PARAMETER :: PROGRAM_NAME = 'LBL2NC'
   CHARACTER(*), PARAMETER :: PROGRAM_RCS_ID = &
   '$Id$'
-  INTEGER     , PARAMETER :: DOWNWELLING = 0
-  INTEGER     , PARAMETER :: UPWELLING   = 1
-  CHARACTER(*), PARAMETER :: DIRECTION_NAME(0:1) = (/ 'downwelling', &
-                                                      'upwelling  ' /)
+  
+  INTEGER,      PARAMETER :: N_DIRECTIONS = 2
+  INTEGER,      PARAMETER :: DOWNWELLING = 0
+  INTEGER,      PARAMETER :: UPWELLING   = 1
+  CHARACTER(*), PARAMETER :: DIRECTION_NAME(0:N_DIRECTIONS-1) = &
+    (/ 'downwelling', 'upwelling  ' /)
   REAL(fp)    , PARAMETER :: ONE = 1.0_fp
 
   ! ---------
@@ -63,10 +69,10 @@ PROGRAM LBL2NC
   INTEGER        :: LBLRTM_FileID
   INTEGER :: Error_Status
   INTEGER :: LBLRTM_EOF
-  INTEGER :: n
+  INTEGER :: i, n, idx(1)
   INTEGER :: n_Frequencies
   INTEGER :: n_Layers
-  INTEGER  :: Direction
+  INTEGER  :: iPanel, iDirection
   REAL(fp) :: f1
   REAL(fp) :: f2
   REAL(fp) :: df
@@ -110,8 +116,32 @@ PROGRAM LBL2NC
   READ(*,'(a)') Id_Tag
   Id_Tag = ADJUSTL(Id_Tag)
 
+  WRITE(*,'(/5x,"Calculation direction")')
+  DO i = 0, N_DIRECTIONS-1
+    WRITE(*,'(7x,i1,") ", a)') i, TRIM(DIRECTION_NAME(i))
+  END DO
+  WRITE(*,FMT='( 5x,"Enter choice: ")',ADVANCE='NO')
+  READ(*,*) iDirection
+  iDirection = MIN(MAX(iDirection,DOWNWELLING),UPWELLING)
+  CALL Display_Message( PROGRAM_NAME, &
+                        TRIM(DIRECTION_NAME(iDirection))//' direction selected.', &
+                        INFORMATION )
 
-  ! --------------------
+  WRITE(*,FMT='(/5x,"Number of panels",&
+               &/7x,"1) single panel",&
+               &/7x,"2) double panel",&
+               &/5x,"Enter choice: ")',&
+          ADVANCE='NO')
+  READ(*,*) iPanel
+  iPanel = MIN(MAX(iPanel,1),2)
+  idx = PACK( (/(i,i=1,LBLRTM_N_PANEL_TYPES)/), &
+              LBLRTM_N_PANELS == iPanel )
+  iPanel = idx(1)
+  CALL Display_Message( PROGRAM_NAME, &
+                        TRIM(LBLRTM_PANEL_TYPE_NAME(iPanel))//' panel file type selected.', &
+                        INFORMATION )
+  
+
   ! Open the LBLRTM file
   ! --------------------
   Error_Status = Open_LBLRTM( TRIM(LBLRTM_Filename), &
@@ -124,82 +154,22 @@ PROGRAM LBL2NC
   END IF
 
 
-
-  ! --------------------------------
-  ! Read the first layer file header
-  ! for direction and frequency data
-  ! --------------------------------
-  Error_Status = Read_LBLRTM_Fhdr( LBLRTM_FileID, &
-                                   LBLRTM_Fhdr, &
-                                   LBLRTM_EOF )
-  IF ( Error_Status /= SUCCESS ) THEN
-    CALL Display_Message( PROGRAM_NAME, &
-                          'Error reading layer #1 file header from file '//&
-                          TRIM(LBLRTM_Filename), &
-                          FAILURE )
-    STOP
-  END IF
-
-  ! Rewind the LBLRTM data file
-  REWIND(LBLRTM_FileID)
-
-  ! Determine the direction value
-  IF ( LBLRTM_Fhdr%Run_Flags%layr1 == LBLRTM_Fhdr%Run_Flags%nlayr ) THEN
-    Direction = UPWELLING
-  ELSE
-    Direction = DOWNWELLING
-  END IF
-  CALL Display_Message( PROGRAM_NAME, &
-                        'Detected calculation direction for '//&
-                        TRIM(LBLRTM_Filename)//' is '//&
-                        TRIM( DIRECTION_NAME( Direction ) ), &
-                        INFORMATION )
-
-  ! Assign the frequency values
-  f1 = REAL(LBLRTM_Fhdr%Begin_Frequency,    fp)
-  f2 = REAL(LBLRTM_Fhdr%End_Frequency,      fp)
-  df = REAL(LBLRTM_Fhdr%Frequency_Interval, fp)
-
-  ! Compute the number of spectral points
-  n_Frequencies = Compute_n_Points(f1,f2,df)
-
-
-  ! -----------------------------
-  ! Create the netCDF format file
-  ! -----------------------------
-  Error_Status = Create_LBLRTM_netCDF( NC_Filename           , &
-                                       n_Frequencies         , &
-                                       Direction             , &
-                                       f1                    , &
-                                       f2                    , &
-                                       df                    , &
-                                       Id_Tag =TRIM(Id_Tag)  , &
-                                       Title  =TRIM(Title)   , &
-                                       History=PROGRAM_RCS_ID, &
-                                       Comment=TRIM(Comment)   )
-  IF ( Error_Status /= SUCCESS ) THEN 
-    CALL Display_Message( PROGRAM_NAME, &
-                          'Error creating '//TRIM(NC_Filename)//'.', &
-                          FAILURE )
-    STOP
-  END IF
-
-
-  ! ---------------------------
   ! Begin loop over data layers
   ! ---------------------------
   WRITE(*,*)
   n_Layers = 0
   Layer_Loop: DO
     n_Layers = n_Layers + 1
-    
+
+
     ! Read the layer
+    ! --------------
     Error_Status = Read_LBLRTM_Layer( LBLRTM_FileID, &
-                                      LBLRTM_SINGLE_PANEL_TYPE, &
+                                      LBLRTM_PANEL_TYPE(iPanel), &
                                       LBLRTM_Layer, &
                                       LBLRTM_EOF )
     IF ( Error_Status /= SUCCESS ) THEN
-      WRITE( Message, '( "Error reading layer #", i3, " from file ", a )' ) &
+      WRITE( Message, '( "Error reading layer #", i0, " from file ", a )' ) &
                       n_Layers, TRIM(LBLRTM_Filename)
       CALL Display_Message( PROGRAM_NAME, &
                             TRIM( Message ), &
@@ -207,47 +177,58 @@ PROGRAM LBL2NC
       STOP
     END IF
     
-    ! Exit loop if EOF reached
-    IF ( LBLRTM_EOF == LBLRTM_FILE_PTR_EOF ) EXIT Layer_loop
-
-
-    ! Check the number of spectral points
-    n = LBLRTM_Layer%n_Points
-    IF ( n /= n_Frequencies ) THEN
-      WRITE( Message, '( "Actual number of LBLRTM points, ", i5, &
-                        &" is different from expected, ", i5, &
-                        &" for layer #", i3, "." )' ) &
-                      n, n_Frequencies, n_Layers
-      CALL Display_Message( PROGRAM_NAME, &
-                            TRIM( Message ), &
-                            FAILURE )
-      STOP
+    
+    ! Create the netCDF file
+    ! ----------------------
+    IF ( n_Layers == 1 ) THEN
+      Error_Status = Create_LBLRTM_netCDF( NC_Filename                    , & 
+                                           LBLRTM_Layer%n_Points          , & 
+                                           iDirection                     , & 
+                                           LBLRTM_Layer%Begin_Frequency   , &
+                                           LBLRTM_Layer%End_Frequency     , &
+                                           LBLRTM_Layer%Frequency_Interval, &
+                                           Id_Tag =TRIM(Id_Tag)           , &
+                                           Title  =TRIM(Title)            , &
+                                           History=PROGRAM_RCS_ID         , &
+                                           Comment=TRIM(Comment)            )
+      IF ( Error_Status /= SUCCESS ) THEN 
+        CALL Display_Message( PROGRAM_NAME, &
+                              'Error creating '//TRIM(NC_Filename)//'.', &
+                              FAILURE )
+        STOP
+      END IF
     END IF
 
+
     ! Write the layer data to the netCDF file
+    ! ---------------------------------------
+    n = LBLRTM_Layer%n_Points
     Error_Status = Write_LBLRTM_netCDF( NC_Filename, &
                                         Transmittance=REAL(LBLRTM_Layer%Spectrum(1:n,1),fp))
     IF ( Error_Status /= SUCCESS ) THEN
-      WRITE( Message, '( "Error writing layer #", i3, " to file ", a )' ) &
+      WRITE( Message, '( "Error writing layer #", i0, " to file ", a )' ) &
                       n_Layers, TRIM( NC_Filename )
       CALL Display_Message( PROGRAM_NAME, &
                             TRIM( Message ), &
                             FAILURE )
       STOP
     END IF
-    WRITE( *, '( 5x, "Layer #", i3, " written..." )' ) n_Layers
+    WRITE( *, '( 5x, "Layer #", i0, " written..." )' ) n_Layers
 
 
     ! Destroy the LBLRTM_Layer structure for the next read
     Error_Status = Destroy_LBLRTM_Layer( LBLRTM_Layer )
     IF ( Error_Status /= SUCCESS ) THEN 
-      WRITE( Message, '( "Error destroying LBLRTM_Layer structure for layer #", i3, &
+      WRITE( Message, '( "Error destroying LBLRTM_Layer structure for layer #", i0, &
                         &"; Input file ", a )' ) n_Layers, TRIM(LBLRTM_Filename)
       CALL Display_Message( PROGRAM_NAME, &
                             TRIM( Message ), &
                             FAILURE )
       STOP
     END IF
+
+    ! Exit loop if EOF reached
+    IF ( LBLRTM_EOF == LBLRTM_FILE_PTR_EOF ) EXIT Layer_loop
 
   END DO Layer_Loop
 
