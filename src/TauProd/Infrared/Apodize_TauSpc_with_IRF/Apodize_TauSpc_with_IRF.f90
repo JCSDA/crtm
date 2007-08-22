0c1c8cb518721e1bbca647a299f75ff01df9c92e
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2007-08-22 16:08:13 +0000

    src:TauProd/Infrared/Apodize_TauSpc_with_IRF subdirectory.   * Apodize_TauSpc_with_IRF.f90: Removed all the power-of-two interpolation of input     spectra; now just using the spectrum as it comes in. Removed the resampling portion     of the code. Now saving both the real and imaginary results of the FFTs in TauProfile     output - but at the same point spacing as the input spectrum.
    Former-commit-id: f3c742476221e9d3b768a4f3f1bd254c811ef6b7

--- src/TauProd/Infrared/Apodize_TauSpc_with_IRF/Apodize_TauSpc_with_IRF.f90
+++ src/TauProd/Infrared/Apodize_TauSpc_with_IRF/Apodize_TauSpc_with_IRF.f90
@@ -23,7 +23,8 @@ PROGRAM Apodize_TauSpc_with_IRF
                                   Program_Message, Display_Message
   USE TauProfile_Define   , ONLY: TauProfile_type, &
                                   Allocate_TauProfile, &
-                                  Destroy_TauProfile
+                                  Destroy_TauProfile, &
+                                  Assign_TauProfile
   USE TauProfile_netCDF_IO, ONLY: Create_TauProfile_netCDF, &
                                   Write_TauProfile_netCDF
   USE LBLRTM_netCDF_IO    , ONLY: Create_LBLRTM_netCDF, &
@@ -65,8 +66,10 @@ PROGRAM Apodize_TauSpc_with_IRF
   CHARACTER(2000) :: LBLRTM_History
   CHARACTER(2000) :: LBLRTM_Comment
   CHARACTER(2000) :: LBLRTM_Id_Tag
+  CHARACTER(2000) :: Comment
   CHARACTER(256) :: Message
   CHARACTER(256) :: LBLRTM_Filename
+  CHARACTER(256) :: TauProfile_Filename
   CHARACTER(20)  :: c_layer
   INTEGER  :: Error_Status
   INTEGER  :: Allocate_Status
@@ -74,10 +77,8 @@ PROGRAM Apodize_TauSpc_with_IRF
   INTEGER  :: if1, if2
   INTEGER  :: ib1, ib2
   INTEGER  :: n_in, n_out
-  INTEGER  :: po2, n_po2
   INTEGER  :: n_filter
-  INTEGER  :: n_spcin
-  INTEGER  :: n_spc_po2, n_ifg_po2
+  INTEGER  :: n_spc, n_ifg
   INTEGER  :: n_layers, dirn
   REAL(fp) :: bf, ef, df
   REAL(fp) :: f1, f2
@@ -86,14 +87,12 @@ PROGRAM Apodize_TauSpc_with_IRF
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: irf     ! The instrument response function
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: f       ! I/P to SPC->IFG FFT; O/P from IFG->SPC FFT
   REAL(fp),    DIMENSION(:), ALLOCATABLE :: spc     ! I/P to SPC->IFG FFT
-  REAL(fp),    DIMENSION(:), ALLOCATABLE :: opd     ! O/P from SPC->IFG FFT; I/P to IFG->SPC FFT
+  REAL(fp),    DIMENSION(:), ALLOCATABLE :: x       ! O/P from SPC->IFG FFT; I/P to IFG->SPC FFT
   COMPLEX(fp), DIMENSION(:), ALLOCATABLE :: ifg     ! O/P from SPC->IFG FFT; I/P to IFG->SPC FFT
   COMPLEX(fp), DIMENSION(:), ALLOCATABLE :: cspc    ! O/P from IFG->SPC FFT
-  REAL(fp), DIMENSION(IASI_MAX_NCHANNELS) :: f_IASI
   INTEGER :: n1, n2, n_IASI_channels
-  INTEGER , DIMENSION(:), ALLOCATABLE :: f_idx
-  REAL(fp), DIMENSION(:), ALLOCATABLE :: spc_IASI
-  TYPE(TauProfile_type) :: TauProfile
+  TYPE(TauProfile_type), TARGET  :: realTau, imagTau
+  TYPE(TauProfile_type), POINTER :: TauProfile => NULL()
   INTEGER :: ik, il
 
 
@@ -117,7 +116,7 @@ PROGRAM Apodize_TauSpc_with_IRF
   LBLRTM_Filename = ADJUSTL(LBLRTM_Filename)
   
   Error_Status = Inquire_LBLRTM_netCDF( LBLRTM_Filename                  , &
-                                        n_Frequencies     =n_spcin       , &
+                                        n_Frequencies     =n_spc         , &
                                         n_Layers          =n_layers      , &
                                         Direction         =dirn          , &
                                         Begin_Frequency   =bf            , &
@@ -139,7 +138,7 @@ PROGRAM Apodize_TauSpc_with_IRF
            &/5x,"bf   = ",es13.6," cm^-1",&
            &/5x,"ef   = ",es13.6," cm^-1",&
            &/5x,"df   = ",es13.6," cm^-1",/)') &
-           n_spcin, n_layers, dirn, bf, ef, df
+           n_spc, n_layers, dirn, bf, ef, df
 
 
   ! -------------------------------------------
@@ -150,27 +149,23 @@ PROGRAM Apodize_TauSpc_with_IRF
   filter_width = REAL(n_filter-1,fp) * df
   WRITE(*,'(/5x,"Rollof filter width: ",es13.6," cm^-1",/)') filter_width
   
-  ! Compute the spectral and interferogram
-  ! lengths for the filtered input spectrum 
-  po2       = ComputeNextPO2(n_spcin)  ! Next power-of-two for input, filtered SPC
-  n_spc_po2 = 2**po2 + 1               ! No. of FFT SPC points
-  n_ifg_po2 = ComputeNIFG(n_spc_po2)   ! No. of FFT IFG points
+  ! Compute interferogram length for the input spectrum 
+  n_ifg = ComputeNIFG(n_spc)   ! No. of FFT IFG points
   
-print *, 'No. of filtered input SPC points: ', n_spcin
-print *, 'No. of SPC points for SPC<->IFG:  ', n_spc_po2, '(',po2,')'
-print *, 'No. of IFG points for SPC<->IFG:  ', n_ifg_po2
+print *, 'No. of input SPC points        : ', n_spc
+print *, 'No. of IFG points for SPC<->IFG: ', n_ifg
 
   ! -----------------------
   ! Allocate all the arrays
   ! -----------------------
   ALLOCATE( ffilter(n_filter), &
             bfilter(n_filter), &
-            irf(n_ifg_po2)   , &  ! The instrument response function (IRF) in IFG space.
-            f(n_spc_po2)     , &  ! Input  to   SPC->IFG FFT; Output from IFG->SPC FFT 
-            spc(n_spc_po2)   , &  ! Input  to   SPC->IFG FFT
-            opd(n_ifg_po2)   , &  ! Output from SPC->IFG FFT; Input  to   IFG->SPC FFT
-            ifg(n_ifg_po2)   , &  ! Output from SPC->IFG FFT; Input  to   IFG->SPC FFT
-            cspc(n_spc_po2)  , &  ! Output from IFG->SPC FFT
+            irf(n_ifg)   , &  ! The instrument response function (IRF) in IFG space.
+            f(n_spc)     , &  ! Input  to   SPC->IFG FFT; Output from IFG->SPC FFT 
+            spc(n_spc)   , &  ! Input  to   SPC->IFG FFT
+            x(n_ifg)     , &  ! Output from SPC->IFG FFT; Input  to   IFG->SPC FFT
+            ifg(n_ifg)   , &  ! Output from SPC->IFG FFT; Input  to   IFG->SPC FFT
+            cspc(n_spc)  , &  ! Output from IFG->SPC FFT
             STAT = Allocate_Status )
   IF (Allocate_Status /= 0) THEN
     Error_Status = FAILURE
@@ -185,8 +180,8 @@ print *, 'No. of IFG points for SPC<->IFG:  ', n_ifg_po2
   ! Compute the input frequency grid
   ! --------------------------------
   f1 = bf
-  f2 = f1 + REAL(n_spc_po2-1,fp)*df
-  f = (/ (REAL(i,fp),i=0,n_spc_po2-1) /) / REAL(n_spc_po2-1,fp) 
+  f2 = f1 + REAL(n_spc-1,fp)*df
+  f = (/ (REAL(i,fp),i=0,n_spc-1) /) / REAL(n_spc-1,fp) 
   f = f*(f2-f1) + f1
 
 
@@ -206,7 +201,7 @@ print *, 'No. of IFG points for SPC<->IFG:  ', n_ifg_po2
   END IF
 
   ! The back end
-  ib1 = n_spcin-n_filter+1; ib2 = n_spcin
+  ib1 = n_spc-n_filter+1; ib2 = n_spc
   bfilter = ffilter(n_filter:1:-1)
 
   
@@ -214,58 +209,41 @@ print *, 'No. of IFG points for SPC<->IFG:  ', n_ifg_po2
   ! Compute the FTS IRF
   ! -------------------
   irf = IASI_GFT(ComputeX(f))
-  
-  ! -------------------------------------
-  ! Compute the IASI resample frequencies
-  ! -------------------------------------
-  f_IASI = IASI_F()
-  n1 = MINLOC( f_IASI, DIM=1, MASK=(f_IASI >= bf) )
-  n2 = MAXLOC( f_IASI, DIM=1, MASK=(f_IASI <= ef) )
-  n_IASI_channels = n2 - n1 + 1
-  
-print *, 'No. of IASI channels:  ', n_IASI_channels
-print *, 'IASI frequency bounds: ', f_IASI(n1), f_IASI(n2)
-  
-  ! Allocate the interpolation arrays
-  ALLOCATE( f_idx(n_IASI_channels), &
-            spc_IASI(n_IASI_channels), &
-            STAT = Allocate_Status )
-  IF (Allocate_Status /= 0) THEN
-    Error_Status = FAILURE
-    WRITE(Message,'("Error allocating interpolation arrays. STAT=",i0)') Allocate_Status
-    CALL Display_Message( PROGRAM_NAME, &
-                          TRIM(Message), &
-                          Error_Status )
-    STOP
-  END IF
-
-  ! Compute the IASI interpolation indices
-  f_idx = IASI_Resample_Idx( f, f_IASI(n1:n2) )
 
 
-  ! ---------------------------------
-  ! Allocate the TauProfile structure
-  ! ---------------------------------
+  ! --------------------------------
+  ! Set up the TauProfile structures
+  ! --------------------------------
+  ! Allocate the structure for the REAL result
   Error_Status = Allocate_TauProfile( n_layers, &
-                                      n_IASI_channels, &
+                                      n_spc, &
                                       1, & ! n_Angles       
                                       1, & ! n_Profiles     
                                       1, & ! n_Molecule_Sets
-                                      TauProfile )  
+                                      realTau )  
   IF ( Error_Status /= SUCCESS ) THEN
     CALL Display_Message( PROGRAM_NAME, &
-                          'Error allocating TauProfile', &
+                          'Error allocating realTau', &
                           Error_Status )
     STOP
   END IF
   
   ! Assign the dimension arrays
-  TauProfile%Sensor_ID      = 'iasi_metopa'
-  TauProfile%Level_Pressure = (/(REAL(ik,fp),ik=0,n_layers)/)
-  TauProfile%Channel        = (/(il+n1-1,il=1,n_IASI_channels)/)
-  TauProfile%Angle          = (/1.5_fp/)
-  TauProfile%Profile        = (/1/)
-  TauProfile%Molecule_Set   = (/10/)
+  realTau%Sensor_ID      = 'iasi_metopa'
+  realTau%Level_Pressure = (/(REAL(ik,fp),ik=0,n_layers)/)
+  realTau%Channel        = (/(il,il=1,n_spc)/)
+  realTau%Angle          = (/1.5_fp/)
+  realTau%Profile        = (/1/)
+  realTau%Molecule_Set   = (/10/)
+  
+  ! Copy the REAL strcuture to the IMAGINARY one
+  Error_Status = Assign_TauProfile( realTau, imagTau )
+  IF ( Error_Status /= SUCCESS ) THEN
+    CALL Display_Message( PROGRAM_NAME, &
+                          'Error copying realTau', &
+                          Error_Status )
+    STOP
+  END IF
 
 
   ! ------------------------------------
@@ -276,15 +254,19 @@ print *, 'IASI frequency bounds: ', f_IASI(n1), f_IASI(n2)
     WRITE(c_layer,'(i0)') k
     WRITE(*,'(5x,"Processing layer # ",i0)') k
 
+
     ! Initialize arrays
+    ! -----------------
     spc = ZERO
     ifg = CMPLX(ZERO,ZERO,fp)
 
+
     ! Read a layer of data, slotting it into the
     ! spc array to be bookended with rolloff filter
+    ! ---------------------------------------------
     Error_Status = Read_LBLRTM_netCDF( LBLRTM_Filename, &
                                        k, &
-                                       Transmittance=spc(1:n_spcin) )
+                                       Transmittance=spc(1:n_spc) )
     IF ( Error_Status /= SUCCESS ) THEN
       CALL Display_Message( PROGRAM_NAME, &
                             'Error reading layer '//TRIM(c_layer)//&
@@ -293,13 +275,17 @@ print *, 'IASI frequency bounds: ', f_IASI(n1), f_IASI(n2)
       STOP
     END IF
 
+
     ! Apply rollof filter to spectrum
+    ! -------------------------------
     spc(if1:if2) = spc(if1:if2) * ffilter  ! Front-end
     spc(ib1:ib2) = spc(ib1:ib2) * bfilter  ! Back-end
 
+
     ! FFT filtered input spectrum to an interferogram
-    Error_Status = SPCtoIFG(f  , spc, &  ! Input
-                            opd, ifg  )  ! Output
+    ! -----------------------------------------------
+    Error_Status = SPCtoIFG(f, spc, &  ! Input
+                            x, ifg  )  ! Output
     IF ( Error_Status /= SUCCESS ) THEN
       CALL Display_Message( PROGRAM_NAME, &
                             'SPC->IFG FFT failed for layer '//TRIM(c_layer), &
@@ -307,33 +293,31 @@ print *, 'IASI frequency bounds: ', f_IASI(n1), f_IASI(n2)
       STOP
     END IF
     
+    
     ! Apply apodisation function to IFG
+    ! ---------------------------------
     ifg = ifg * irf
     
+    
     ! FFT truncated interferogram to a spectrum
-    Error_Status = IFGtoSPC(opd, ifg,  &  ! Input
-                            f  , cspc  )  ! Output
+    ! -----------------------------------------
+    Error_Status = IFGtoSPC(x, ifg,  &  ! Input
+                            f, cspc  )  ! Output
     IF ( Error_Status /= SUCCESS ) THEN
       CALL Display_Message( PROGRAM_NAME, &
                             'IFG->FFT FFT failed for layer '//TRIM(c_layer), &
                             Error_Status )
       STOP
     END IF
-    f = f + bf  ! Restore begin frequency
     
-    ! Resample result to IASI frequencies
-    Error_Status = IASI_Resample( f, REAL(cspc,fp), f_IASI(n1:n2), &
-                                  spc_IASI, &
-                                  f_idx = f_idx )
-    IF ( Error_Status /= SUCCESS ) THEN
-      CALL Display_Message( PROGRAM_NAME, &
-                            'IASI resample failed for layer '//TRIM(c_layer), &
-                            Error_Status )
-      STOP
-    END IF
+    ! Restore begin frequency
+    f = f + bf 
+    
 
-    ! Copy result to TauProfile structure
-    TauProfile%Tau(k,:,1,1,1) = spc_IASI
+    ! Copy results to TauProfile structures
+    ! -------------------------------------
+    realTau%Tau(k,:,1,1,1) = REAL(cspc,fp)
+    imagTau%Tau(k,:,1,1,1) = AIMAG(cspc)
 
   END DO Layer_loop
 
@@ -342,56 +326,77 @@ print *, 'IASI frequency bounds: ', f_IASI(n1), f_IASI(n2)
   ! Write spectrally reducted data
   ! to TauProfile format file
   ! ------------------------------
-  ! Create the output file
-  Error_Status = Create_TauProfile_netCDF( 'iasi_metopa.TauProfile.nc'   , &  ! Input
-                                           TauProfile%Level_Pressure  , &  ! Input
-                                           TauProfile%Channel         , &  ! Input
-                                           TauProfile%Angle           , &  ! Input
-                                           TauProfile%Profile         , &  ! Input
-                                           TauProfile%Molecule_Set    , &  ! Input
-                                           Sensor_ID       =TauProfile%Sensor_ID , &  ! Optional Input
-                                           Sensor_Name     ='IASI'          , &  ! Optional input
-                                           Platform_Name   ='MetOp-A'       , &  ! Optional input
-                                           ID_Tag      = TRIM(LBLRTM_ID_Tag)          , &  ! Optional input
-                                           Title       = 'IASI transmittance profiles', &  ! Optional input
-                                           History     = TRIM(LBLRTM_History)//'; '//  &
-                                                         PROGRAM_RCS_ID               , &  ! Optional input
-                                           Comment     = 'Test file'                    )  ! Optional input
-  IF ( Error_Status /= SUCCESS ) THEN
-    CALL Display_Message( PROGRAM_NAME, &
-                          'Output file creation failed', &
-                          Error_Status )
-    STOP
-  END IF
+  DO i = 1, 2
+  
+    ! Assign values for different TauProfiles
+    IF ( i == 1 ) THEN
+      TauProfile_Filename = TRIM(realTau%Sensor_ID)//'.REAL.TauProfile.nc'
+      Comment = 'REAL part of result'
+      TauProfile => realTau
+    ELSE
+      TauProfile_Filename = TRIM(imagTau%Sensor_ID)//'.IMAG.TauProfile.nc'
+      Comment = 'IMAGINARY part of result'
+      TauProfile => imagTau
+    END IF
+    
+    ! Create the output file
+    Error_Status = Create_TauProfile_netCDF( TauProfile_Filename        , &  ! Input
+                                             TauProfile%Level_Pressure  , &  ! Input
+                                             TauProfile%Channel         , &  ! Input
+                                             TauProfile%Angle           , &  ! Input
+                                             TauProfile%Profile         , &  ! Input
+                                             TauProfile%Molecule_Set    , &  ! Input
+                                             Sensor_ID   = TauProfile%Sensor_ID , &  ! Optional Input
+                                             ID_Tag      = TRIM(LBLRTM_ID_Tag)  , &  ! Optional input
+                                             Title       = 'IASI transmittance profiles', &  ! Optional input
+                                             History     = TRIM(LBLRTM_History)//'; '//  &
+                                                           PROGRAM_RCS_ID       , &  ! Optional input
+                                             Comment     = TRIM(Comment)          )  ! Optional input
+    IF ( Error_Status /= SUCCESS ) THEN
+      CALL Display_Message( PROGRAM_NAME, &
+                            'Creation of output file '//TRIM(TauProfile_Filename)//' failed', &
+                            Error_Status )
+      STOP
+    END IF
 
-  ! Output the entire TauProfile structure   
-  Error_Status = Write_TauProfile_netCDF( 'iasi_metopa.TauProfile.nc', &  ! Input
-                                          TauProfile                   )  ! Optional input
-  IF ( Error_Status /= SUCCESS ) THEN
-    CALL Display_Message( PROGRAM_NAME, &
-                          'Error writing TauProfile to file', &
-                          Error_Status )
-    STOP
-  END IF
+    ! Output the entire TauProfile structure   
+    Error_Status = Write_TauProfile_netCDF( TauProfile_Filename, &  ! Input
+                                            TauProfile           )  ! Optional input
+    IF ( Error_Status /= SUCCESS ) THEN
+      CALL Display_Message( PROGRAM_NAME, &
+                            'Error writing to '//TRIM(TauProfile_Filename), &
+                            Error_Status )
+      STOP
+    END IF
 
+    ! Nullify the pointer
+    NULLIFY(TauProfile)
+    
+  END DO
+  
 
   ! --------
   ! Clean up
   ! --------
   ! Destroy the TauProfile structure
-  Error_Status = Destroy_TauProfile( TauProfile )
+  Error_Status = Destroy_TauProfile( realTau )
   IF ( Error_Status /= SUCCESS ) THEN
     CALL Display_Message( PROGRAM_NAME, &
-                          'Error destroying TauProfile structure', &
+                          'Error destroying realTau structure', &
+                          WARNING )
+  END IF
+  Error_Status = Destroy_TauProfile( imagTau )
+  IF ( Error_Status /= SUCCESS ) THEN
+    CALL Display_Message( PROGRAM_NAME, &
+                          'Error destroying imagTau structure', &
                           WARNING )
   END IF
   ! Deallocate arrays
   DEALLOCATE( ffilter, bfilter, &
               irf             , &
               f      , spc    , &
-              opd    , ifg    , &
+              x      , ifg    , &
               cspc   , &
-              f_idx  , spc_IASI, &
               STAT = Allocate_Status )
   IF ( Allocate_Status /= 0 ) THEN
     WRITE( Message,'("Error deallocating arrays. STAT = ",i0)') Allocate_Status
