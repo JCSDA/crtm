29a8bbbb9705a955427829b762c3da9a42c635c1
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2007-09-28 23:34:08 +0000

    scripts:ruby/tauprod/lib/IASI subdirectory.   * iasi_tauprod.rb: Added options processing.
    Former-commit-id: a07f095d25177fe4b1384e6137fabc63e64e335c

--- scripts/ruby/tauprod/lib/IASI/iasi_tauprod.rb
+++ scripts/ruby/tauprod/lib/IASI/iasi_tauprod.rb
@@ -1,22 +1,127 @@
 #!/usr/bin/env ruby
+#
+# == Synopsis
+#
+# iasi_tauprod.rb::  Generate and submit the IASI TauProd processing scripts.
+#
+# == Usage
+#
+# iasi_tauprod.rb [OPTIONS]
+#
+#
+# --help  (-h):
+#    you're looking at it.
+#
+# --noop  (-n):
+#    Create all the directories and script files, but do not submit any jobs
+#    to the batch processor.
+#
+# --angles a1[-a2][,a3,a4,..]  (-a):
+#    Specify the angles to process. The angles can be specified individually,
+#    a1,a2,a3,... or as ranges, a1-a2,a3-a4, or combinations thereof, a1-a2,a3,a4.
+#    Valid values are:
+#      1 == SEC(z)=1.00, z= 0.000
+#      2 == SEC(z)=1.25, z=36.870
+#      3 == SEC(z)=1.50, z=48.190
+#      4 == SEC(z)=1.75, z=55.150
+#      5 == SEC(z)=2.00, z=60.000
+#      6 == SEC(z)=2.25, z=63.612
+#      7 == SEC(z)=3.00, z=70.529
+#
+# --bands b1[-b2][,b3,b4,..]  (-b):
+#    Specify the IASI bands to process. The bands can be specfied individually
+#    b1,b2,b3,... or as a range, b1-b3.
+#
+# --molid m1[-m2][,m3,m4,..]  (-m):
+#    Specify the molecule sets to check. The molecule set ids can be specified
+#    individually, m1,m2,m3,... or as ranges, m1-m2,m3-m4, or combinations
+#    thereof, m1-m2,m3,m4.
+#    Valid values are:
+#       1-7 == individual molecule numbers, no continua
+#       8   == all first seven molecules, no continua
+#       9   == continua only
+#      10   == all first seven molecules and their continua
+#      11   == water vapor + ozone only and their continua
+#      12   == water vapor only and it's continua
+#      13   == dry gases. Everything but h2o and o3 and their continua
+#      14   == ozone only and it's continua
+#      15   == water vapor continua only
+#
+# --profiles p1[-p2][,p3,p4,..]  (-p):
+#    Specify the profiles to process. The profiles can be specified individually,
+#    p1,p2,p3,... or as ranges, p1-p2,p3-p4, or combinations thereof, p1-p2,p3,p4.
+#    
+# --co2_mr ppmv  (-c):
+#    The CO2 mixing ratio, in ppmv, to use in the LBLRTM calculations. If not
+#    specified the default value is 380.0ppmv.
+#
+# --start_delay min  (-i):
+#    The initial delay, in minutes, for which jobs are put on hold in the
+#    the batch processor. If not specified, the default is 5 minutes.
+#
+# --queue name  (-q):
+#    Specify the batch queue to which the generated script file will be submitted.
+#
+# --profile_set name  (-s):
+#    Specify the profile set being used.
+#    Valid profile set names are:
+#      UMBC  (DEFAULT)
+#      ECMWF
+#      CIMSS
+#
+# --t5_dir dirname  (-d):
+#    Specify the location of the LBLRTM TAPE5 input files.
+#
+# --t3_id name  (-t):
+#    Specify the TAPE3 spectroscopic database to use in the LBLRTM calculations.
+#    Valid id names are:
+#      hitran2004     == the HITRAN 2004 database
+#      hitran2000_aer == the HITRAN 2000 database with AER updates (DEFAULT)
+#      hitran2000     == the HITRAN 2000 database
+#      hitran1996_jpl == the HITRAN 1996 database with JPL/Toth updates
+#
+#
+# Written by:: Paul van Delst, CIMSS/SSEC 27-Sep-2007 (paul.vandelst@noaa.gov)
+#
 
-# ------------
-# Set up stuff
-# ------------
+require 'getoptlong'
+require 'rdoc/usage'
+
+
+# Define methods
+# --------------
+# Method to parse input range definitions
+# e.g. 1-3,6,10-12  produces [1,2,3,6,10,11,12]
+def parse_range(input)
+  input.split(",").collect do |element|
+    if element =~ /(\d+)-(\d+)/
+      ($1.to_i..$2.to_i).to_a
+    else
+      element.to_i
+    end
+  end.flatten.uniq.sort
+end
+
+# Method to replace only the first
+# occurance of the leading spaces
+# in each line of input text.
+def strip_output(text)
+  text =~ /^\s+/
+  leading_spaces = $&
+  text = text.to_a.collect {|l| l.sub(/^#{leading_spaces}/,"")}.to_s
+end
+
+
+# Define the constants
+# --------------------
 LL_SUBMIT    = "llsubmit"
-LL_QUEUE     = "dev"
 LL_ACCOUNT   = { :ccs=>"GDAS-T2O", :haze=>"JCSDA001-RES"}
 LL_TIMELIMIT = "03:00:00"
 LL_RESOURCES = "ConsumableCpus(1) ConsumableMemory(500)"
 
 SUBMIT_INCREMENT = 60*60*3  # seconds
 
-TAPE5_DIR = "./TAPE5_files"
-PROFILE_SET_ID = "UMBC"
-PROFILE_SET = 1
-TAPE3_ID = "hitran2000_aer"
 LBLRTM_HITRAN_VERSION = "LBLRTM v9.4; HITRAN 2000 + AER updates"
-CO2MR   = 380.0  # Default CO2 mixing ratio in ppmv
 DFAVG   = 0.001  # Averaging kernel width in cm^-1
 NPANELS = 1      # No. of LBL panels
 UPDIRN  = 1      # Direction flag for upwelling
@@ -26,6 +131,32 @@ SENSOR_ID = "iasi_metopa"
 # Literal constants
 ZERO=0.0; ONE=1.0
 
+
+# -------------------
+# Define the defaults
+# -------------------
+# Default processing queue
+queue = "dev"
+
+# Start delay
+start_delay = 60*5  # seconds
+
+# Default location of the LBLRTM input files
+t5_dir = "./TAPE5_files"
+
+# Default spectroscopic database
+t3_id = "hitran2000_aer"
+
+# Default CO2 mixing ratio
+co2_mr = 380.0
+
+# Profile set info
+PROFILE_SET_INFO = { "UMBC"  => {:id => 1, :n_profiles => 48},
+                     "ECMWF" => {:id => 2, :n_profiles => 52},
+                     "CIMSS" => {:id => 3, :n_profiles => 32}}
+profile_set = "UMBC"
+profiles = (1..PROFILE_SET_INFO[profile_set][:n_profiles]).to_a
+                   
 # Band information. The SCNMRG frequencies are those values that,
 # for a point spacing of 0.001cm^-1, produce a number of points
 # that has only prime factors of 2, 3, and 5.
@@ -75,35 +206,81 @@ ANGLE_INFO = { 1 => {:secant => 1.00, :angle => 0.0   , :zenith => 180.000},
                7 => {:secant => 3.00, :angle => 70.529, :zenith => 109.471} }
 angles = ANGLE_INFO.keys.sort
 
-PROFILE1 = 1
-PROFILE2 = 48
-profiles = (PROFILE1..PROFILE2).collect {|x| x}
+# No debug output by default
+debug = false
 
-# Method to replace only the first
-# occurance of the leading spaces
-# in each line of input text.
-def strip_output(text)
-  text =~ /^\s+/
-  leading_spaces = $&
-  text = text.to_a.collect {|l| l.sub(/^#{leading_spaces}/,"")}.to_s
+# Submit jobs by default
+noop = false
+
+
+# Parse the command line options
+# ------------------------------
+# Specify accepted options
+options=GetoptLong.new(
+  [ "--help"       , "-h", GetoptLong::NO_ARGUMENT       ],
+  [ "--debug"      , "-g", GetoptLong::NO_ARGUMENT       ],
+  [ "--noop"       , "-n", GetoptLong::NO_ARGUMENT       ],
+  [ "--angles"     , "-a", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--bands"      , "-b", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--co2_mr"     , "-c", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--start_delay", "-i", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--molid"      , "-m", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--profiles"   , "-p", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--queue"      , "-q", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--profile_set", "-s", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--t5_dir"     , "-d", GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--t3_id"      , "-t", GetoptLong::REQUIRED_ARGUMENT ] )
+
+# Process the arguments
+begin
+  options.each do |opt, arg|
+    case opt
+      when "--help"
+        RDoc::usage
+        exit 0
+      when "--debug"
+        debug = true
+      when "--noop"
+        noop = true
+      when "--angles"
+        angles = parse_range(arg)
+      when "--bands"
+        bands = parse_range(arg)
+      when "--molid"
+        molids = parse_range(arg)
+      when "--profiles"
+        profiles = parse_range(arg)
+      when "--co2_mr"
+        co2_mr = arg.to_f
+      when "--start_delay"
+        start_delay = (arg.to_i)*60  # Convert to seconds
+      when "--queue"
+        queue = arg
+      when "--profile_set"
+        profile_set = arg if PROFILE_SET_INFO.has_key?(arg)
+      when "--t5_dir"
+        t5_dir = arg
+      when "--t3_id"
+        t3_id = arg
+    end
+  end
+rescue StandardError => error_message
+  puts("\nERROR: #{error_message}\n")
+  RDoc::usage
+  exit 1
 end
 
 
-# ----------------------------------
-# The start of the script processing
-# ----------------------------------
-if $0 == __FILE__
-
-# Define the current directory
-root_dir = Dir.pwd
-
 # Begin the error handling block
+# ------------------------------
 begin
 
+  # Define the current directory
+  root_dir = Dir.pwd
+
   # Get the initial submit time
-  # ---------------------------
-  dtime = 60*5
-  time  = Time.now + dtime
+  time  = Time.now + start_delay
+
   
   # Begin the band loop
   # -------------------
@@ -134,7 +311,7 @@ begin
       continua = "%3.1f "*minfo[:cont].size % minfo[:cont]
     
       # Construct the tape3 filename
-      t3file = "tape3.#{minfo[:t3tag]}.#{TAPE3_ID}"
+      t3file = "tape3.#{minfo[:t3tag]}.#{t3_id}"
       
       
       # Begin the profile loop
@@ -146,7 +323,7 @@ begin
         system(cmd) unless File.directory?(pdir)
       
         # Construct the generic TAPE5 filename 
-        gt5file="#{TAPE5_DIR}/TAPE5.#{PROFILE_SET_ID}_#{pname}"
+        gt5file="#{t5_dir}/TAPE5.#{profile_set}_#{pname}"
         
         
         # Create a Job Control File for this band/molid combination
@@ -160,8 +337,8 @@ begin
           # @ job_name = #{bname}_#{mname}_#{pname}
           # @ output = #{bname}_#{mname}_#{pname}.out
           # @ error = #{bname}_#{mname}_#{pname}.err
-          # @ class = #{LL_QUEUE}
-          # @ group = #{LL_QUEUE}
+          # @ class = #{queue}
+          # @ group = #{queue}
           # @ wall_clock_limit = #{LL_TIMELIMIT}
           # @ resources = #{LL_RESOURCES}
           # @ account_no = #{LL_ACCOUNT[:ccs]}
@@ -199,7 +376,7 @@ begin
           # Every thing else, replace all occurrances
           replace=[[/UUUU.UUU/ ,"%8.3f"%f1_lbl],               # LBL begin frequency
                    [/VVVV.VVV/ ,"%8.3f"%f2_lbl],               # LBL end frequency
-                   [/CCC.CCC/  ,"%7.3f"%CO2MR],                # CO2 mixing ratio
+                   [/CCC.CCC/  ,"%7.3f"%co2_mr],               # CO2 mixing ratio
                    [/AAA.AAA/  ,"%7.3f"%ainfo[:zenith]],       # Zenith angle
                    [/C.CCCC/   ,"%6.4f"%(DFAVG/2.0)],          # Averaging kernel halfwidth
                    [/D.DDDD/   ,"%6.4f"%DFAVG],                # Output spectral spacing
@@ -235,7 +412,7 @@ begin
             
             ID_TAG="#{id_tag}"
             ID_ATTRIBUTE="#{id_att}"
-            PROFILE_SET_ID="#{PROFILE_SET_ID}"
+            PROFILE_SET="#{profile_set}"
             LBLRTM_HITRAN_VERSION="#{LBLRTM_HITRAN_VERSION}"
             SENSOR_ID="#{SENSOR_ID}"
             
@@ -300,9 +477,9 @@ begin
             # Upwelling
             LBL2NC << NoMoreInput
             ${UP_LBL_FILE}
-            ${ID_ATTRIBUTE} Transmittance production run for ${PROFILE_SET_ID} profile set.
+            ${ID_ATTRIBUTE} Transmittance production run for ${PROFILE_SET} profile set.
             Upwelling (Layer->TOA) transmittance. ${LBLRTM_HITRAN_VERSION}
-            ${PROFILE_SET_ID}
+            ${PROFILE_SET}
             #{UPDIRN}
             #{NPANELS}
             NoMoreInput
@@ -316,9 +493,9 @@ begin
             # Downwelling
             LBL2NC << NoMoreInput
             ${DOWN_LBL_FILE}
-            ${ID_ATTRIBUTE} Transmittance production run for ${PROFILE_SET_ID} profile set.
+            ${ID_ATTRIBUTE} Transmittance production run for ${PROFILE_SET} profile set.
             Downwelling (Layer->SFC) transmittance. ${LBLRTM_HITRAN_VERSION}
-            ${PROFILE_SET_ID}
+            ${PROFILE_SET}
             #{DNDIRN}
             #{NPANELS}
             NoMoreInput
@@ -336,7 +513,7 @@ begin
             # Upwelling
             Apodize_TauSpc_with_IRF << NoMoreInput
             ${UP_NC_FILE}
-            #{PROFILE_SET}
+            #{PROFILE_SET_INFO[profile_set][:id]}
             #{m}
             #{p}
             #{a}
@@ -356,7 +533,7 @@ begin
             # Downwelling
             Apodize_TauSpc_with_IRF << NoMoreInput
             ${DOWN_NC_FILE}
-            #{PROFILE_SET}
+            #{PROFILE_SET_INFO[profile_set][:id]}
             #{m}
             #{p}
             #{a}
@@ -398,7 +575,7 @@ begin
         # Submit the job command file
         jcf.close
         cmd = "#{LL_SUBMIT} #{jcf_file}"
-        system(cmd)
+        system(cmd) unless noop
 
       end # profile loop
 
@@ -410,9 +587,7 @@ begin
     end # molecule loop
   end # band loop                
    
-rescue StandardError=>error_message
-  puts("\nERROR--#{error_message}\n")
+rescue StandardError => error_message
+  puts("\nERROR: #{error_message}\n")
   exit 1
 end  # error handling block
-
-end
