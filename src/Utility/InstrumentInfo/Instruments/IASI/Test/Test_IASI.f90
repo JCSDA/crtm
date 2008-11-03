d6ee82526c8e01cf4ad11959c8ed9ccb3e5f991a
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2008-11-03 19:23:13 +0000

    src:Utility/InstrumentInfo/Instruments/IASI/Test subdirectory.   * Test_IASI.f90: Modified to produce IFGs and SRFs for the different point numbering separately.     (Write_File): Added to produce standard output XY BinFiles.   * test_iasi.pro: Modified to plot new point numbering IFGs and SRFs. Added include file color_db     to use color names rather than index values.
    Former-commit-id: 600f494d7f2aeb263342aecfd35a50a550ef3aef

--- src/Utility/InstrumentInfo/Instruments/IASI/Test/Test_IASI.f90
+++ src/Utility/InstrumentInfo/Instruments/IASI/Test/Test_IASI.f90
@@ -1,86 +1,154 @@
 PROGRAM Test_IASI
 
-  USE Type_Kinds           , ONLY: fp=>fp_kind
+  USE Type_Kinds           , ONLY: fp
   USE Binary_File_Utility  , ONLY: Open_Binary_File
   USE Message_Handler      , ONLY: SUCCESS
   USE Fundamental_Constants, ONLY: PI
   USE IASI_Define          , ONLY: IASI_X, IASI_GFT
-  USE FFT_Spectral_Utility , ONLY: IFGtoSPC
+  USE FFT_Spectral_Utility , ONLY: ComputeNSPC, ComputeMeanDelta, &
+                                   IFGtoSPC
   IMPLICIT NONE
   ! Parameters
-  INTEGER, PARAMETER, DIMENSION(2) :: N=(/51200, 2**18/)
+  INTEGER , PARAMETER :: N_FFT = 51200
+  INTEGER , PARAMETER :: N_PO2 = 2**19
+  REAL(fp), PARAMETER :: ZERO  = 0.0_fp
   ! Variables
-  CHARACTER(256) :: outFile
-  REAL(fp),    DIMENSION(:), ALLOCATABLE :: x, gft
-  REAL(fp),    DIMENSION(:), ALLOCATABLE :: f
-  COMPLEX(fp), DIMENSION(:), ALLOCATABLE :: srf
-  INTEGER :: i, fileId, nSRF
-  INTEGER :: errStatus, allocStatus
+  REAL(fp)   , ALLOCATABLE :: x(:), gft(:)
+  REAL(fp)   , ALLOCATABLE :: f(:)
+  COMPLEX(fp), ALLOCATABLE :: srf(:)
+  REAL(fp) :: dX, maxX
+  INTEGER  :: i
+  INTEGER  :: n_IFG, n_SPC
+  INTEGER  :: nHalf, i1, i2
+  INTEGER  :: Error_Status, Allocate_Status
   
-  ! Loop over test sizes
-  DO i = 1, SIZE(N)
   
-    ! The number of SRF points (+ve frequencies only)
-    nSRF = (N(i)/2)+1
-    
-    ! Allocate arrays
-    ALLOCATE(x(N(i)), gft(N(i)), &
-             f(nSRF), srf(nSRF), &
-             STAT=allocStatus)
-    IF ( allocStatus /= 0 ) THEN
-      WRITE(*,*) 'Error allocating arrays. STAT=',allocStatus
-      STOP
-    END IF
-    
-    ! Compute optical delay grid
-    x = IASI_X(N(i))
-    
-    ! Compute the GFT
-    gft = IASI_GFT(x)
-    
-    ! FFT GFT to a spectrum
-    errStatus=IFGtoSPC(x, gft, f, srf)
-    IF ( errStatus /= SUCCESS ) THEN
-      WRITE(*,*) 'Error in IFGtoSPC call'
-      STOP
-    END IF
+  ! Compute the nominal IASI GFT
+  ! ----------------------------
+  ! The number of IFG points
+  n_IFG = N_FFT
+  ! The number of SRF points (+ve frequencies only)
+  n_SPC = ComputeNSPC(n_IFG)
+  
+  ! Allocate arrays
+  ALLOCATE(x(n_IFG), gft(n_IFG), &
+           f(n_SPC), srf(n_SPC), &
+           STAT=Allocate_Status)
+  IF ( Allocate_Status /= 0 ) THEN
+    WRITE(*,*) 'Error allocating arrays. STAT=',Allocate_Status
+    STOP
+  END IF
 
-    ! Output the GFT result
-    WRITE(outFile,'("IASI_GFT.",i0,".bin")') N(i)
-    errStatus = Open_Binary_File( outFile, &
-                                  fileId, &
-                                  For_Output=1 )
-    IF ( errStatus /= SUCCESS ) THEN
-      WRITE(*,*) 'Error opening output results file: ',TRIM(outFile)
-      STOP
-    END IF
-    WRITE(fileId) N(i)
-    WRITE(fileId) x
-    WRITE(fileId) gft
-    CLOSE(fileId)
+  ! Compute optical delay grid and output some numbers
+  x = IASI_X(n_IFG)
+  dX = ComputeMeanDelta(x)
+  WRITE( *,'(5x,"-Xmax,+Xmax = ",2(1x,es15.8))' ) x(1),x(n_IFG)
+  WRITE( *,'(5x,"dX = ",es15.8)' ) dX
+  WRITE( *,'(5x,"-Xmax-dX = ",es15.8)' ) x(1)-dX
+  
+  ! Compute the GFT
+  gft = IASI_GFT(x)
+  WRITE( *,'(5x,"GFT(-Xeff),GFT(+Xeff) = ",2(1x,es23.16))' ) gft(1),gft(n_IFG)
+  
+  ! Output the GFT result
+  CALL Write_File(x, gft)
 
-    ! Output the SRF
-    WRITE(outFile,'("IASI_SRF.",i0,".bin")') N(i)
-    errStatus = Open_Binary_File( outFile, &
-                                  fileId, &
-                                  For_Output=1 )
-    IF ( errStatus /= SUCCESS ) THEN
-      WRITE(*,*) 'Error opening output results file: ',TRIM(outFile)
+  ! FFT GFT to a spectrum
+  Error_Status = IFGtoSPC(x, gft, f, srf)
+  IF ( Error_Status /= SUCCESS ) THEN
+    WRITE(*,*) 'Error in IFGtoSPC call'
+    STOP
+  END IF
+
+  ! Output the SRF
+  CALL Write_File(f, REAL(srf,fp), yi=AIMAG(srf))
+  
+  ! Deallocate arrays
+  DEALLOCATE(x, gft, f, srf, STAT=Allocate_Status)
+  IF ( Allocate_Status /= 0 ) THEN
+    WRITE(*,*) 'Error deallocating arrays. STAT=',Allocate_Status
+    STOP
+  END IF
+
+
+  ! Compute a "higher resolution" IASI GFT
+  ! --------------------------------------
+  ! The number of IFG points
+  n_IFG = N_PO2
+  ! The number of SRF points (+ve frequencies only)
+  n_SPC = ComputeNSPC(n_IFG)
+  
+  ! Allocate arrays
+  ALLOCATE(x(n_IFG), gft(n_IFG), &
+           f(n_SPC), srf(n_SPC), &
+           STAT=Allocate_Status)
+  IF ( Allocate_Status /= 0 ) THEN
+    WRITE(*,*) 'Error allocating arrays. STAT=',Allocate_Status
+    STOP
+  END IF
+
+  ! Compute optical delay grid and output some numbers
+  maxX = REAL(N_IFG/2,fp) * dX
+  nHalf = N_IFG/2
+  x(nHalf:N_IFG) = (/(REAL(i,fp),i=0,nHalf)/)/REAL(nHalf,fp)
+  x(1:nHalf-1)   = -x(N_IFG-1:nHalf+1:-1)
+  x = x*maxX
+  WRITE( *,'(5x,"-Xmax,+Xmax = ",2(1x,es15.8))' ) x(1),x(n_IFG)
+  WRITE( *,'(5x,"dX = ",es15.8)' ) dX
+  WRITE( *,'(5x,"-Xmax-dX = ",es15.8)' ) x(1)-dX
+  
+  ! Compute the GFT
+  gft = ZERO
+  i1 = (N_IFG-N_FFT)/2 + 1
+  i2 = i1 + N_FFT - 1
+  gft(i1:i2) = IASI_GFT(x(i1:i2))
+  WRITE( *,'(5x,"GFT(-Xeff),GFT(+Xeff) = ",2(1x,es15.8))' ) gft(i1),gft(i2)
+  
+  ! Output the GFT result
+  CALL Write_File(x, gft)
+
+  ! FFT GFT to a spectrum
+  Error_Status = IFGtoSPC(x, gft, f, srf)
+  IF ( Error_Status /= SUCCESS ) THEN
+    WRITE(*,*) 'Error in IFGtoSPC call'
+    STOP
+  END IF
+
+  ! Output the SRF
+  CALL Write_File(f, REAL(srf,fp), yi=AIMAG(srf))
+  
+  ! Deallocate arrays
+  DEALLOCATE(x, gft, f, srf, STAT=Allocate_Status)
+  IF ( Allocate_Status /= 0 ) THEN
+    WRITE(*,*) 'Error deallocating arrays. STAT=',Allocate_Status
+    STOP
+  END IF
+
+CONTAINS
+
+  SUBROUTINE Write_File(x, yr, yi)
+    REAL(fp),           INTENT(IN) :: x(:), yr(:)
+    REAL(fp), OPTIONAL, INTENT(IN) :: yi(:)
+    INTEGER :: n, FileID, Error_Status
+    CHARACTER(256) :: Outfile
+    n = SIZE(x)
+    IF ( PRESENT(yi) ) THEN
+      WRITE(Outfile,'("IASI_SRF.",i0,".bin")') n
+    ELSE
+      WRITE(Outfile,'("IASI_GFT.",i0,".bin")') n
+    END IF
+    Error_Status = Open_Binary_File( Outfile, &
+                                     FileID, &
+                                     For_Output=1 )
+    IF ( Error_Status /= SUCCESS ) THEN
+      WRITE(*,*) 'Error opening output results file: ',TRIM(Outfile)
       STOP
     END IF
-    WRITE(fileId) nSRF
-    WRITE(fileId) f
-    WRITE(fileId) REAL(srf,fp)
-    WRITE(fileId) AIMAG(srf)
-    CLOSE(fileId)
-    
-    ! Deallocate arrays
-    DEALLOCATE(x, gft, f, srf, STAT=allocStatus)
-    IF ( allocStatus /= 0 ) THEN
-      WRITE(*,*) 'Error deallocating arrays. STAT=',allocStatus
-      STOP
-    END IF
-    
-  END DO
+    WRITE(FileID) n
+    WRITE(FileID) x
+    WRITE(FileID) yr
+    IF ( PRESENT(yi) ) WRITE(FileID) yi
+    CLOSE(FileID)
+  END SUBROUTINE Write_File
 
 END PROGRAM Test_IASI
