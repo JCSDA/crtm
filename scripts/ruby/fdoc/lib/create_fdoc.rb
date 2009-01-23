a89cb13530eb5150e637395841102a4ccad0cf48
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2009-01-23 22:37:59 +0000

    scripts:ruby/fdoc/lib subdirectory.
      * create_fdoc.rb: Modified to use command line arguments as source files.
        Added options processing.
    
    
    
    Former-commit-id: 23140abf03484b31a448b90395d46f222f1cb090

--- scripts/ruby/fdoc/lib/create_fdoc.rb
+++ scripts/ruby/fdoc/lib/create_fdoc.rb
@@ -1,19 +1,55 @@
 #!/usr/bin/env ruby
+#
+# == Synopsis
+#
+# create_fdoc.rb:: Create latex and html documentation from instrumented
+#                  Fortran95 source files.
+#
+# == Usage
+#
+# create_fdoc.rb --help FILE1 [FILE2|...|FILEN]
+#
+# --help (-h):
+#    you're looking at it
+#
+# Written by:: Paul van Delst, 15-Jan-2009 (paul.vandelst@noaa.gov)
+#
 
+
+require 'getoptlong'
+require 'rdoc/usage'
 require 'fdoc'
 
-file_name = "test.f90"
+# Define the script name for messages
+script_name=File.basename($0)
 
-# Get the source
-# --------------
-source = FDoc::Source.load(file_name)
-puts(source.lines.class)
+# Specify accepted options
+options=GetoptLong.new([ "--help", "-h", GetoptLong::NO_ARGUMENT])
 
-i1 = source.lines.index("  !:tdoc+\n")
-i2 = source.lines.index("  !:tdoc-\n")
-puts i1, i2
-puts(source.lines[i1+1..i2-1])
+# Parse the command line options
+begin
+  options.each do |opt, arg|
+    case opt
+      when "--help"
+        RDoc::usage
+        exit(0)
+    end
+  end
+rescue StandardError=>error_message
+  puts("ERROR: #{error_message}")
+  puts("Try \"#{script_name} --help\"\n ")
+  exit(1)
+end
 
-# Begin the processing
-# --------------------
-FDoc::Generator.new.generate(source)
+# Check the arguments
+if ARGV.length == 0
+  puts("\n  #{script_name}: No Fortran source files specified, nothing to do. Exiting...\n\n")
+  exit(0)
+end
+
+# Process the source files
+ARGV.each do |source_file|
+  source = FDoc::Source.load(source_file)
+  FDoc::Generator.new.generate(source)
+end
+
