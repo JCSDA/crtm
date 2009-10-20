0ed03828b02f5cc8cddbddf5cdf54f710daa6a8e
yong.chen@noaa.gov <yong.chen@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2009-10-20 19:33:40 +0000

    src:Coefficients/TauCoeff/ODPS subdirectory.
     *ODPS_NC2BIN/sensor_list:
     *ODPS_NC2BIN/run_ODPS_NC2BIN.sh: script to run convert the ODPS TauCoeff netCDF format to Binary format.
    
    
    
    
    Former-commit-id: 2132acf22c5be582506e1ad8b45022b6a3451b9f

---
+++ src/Coefficients/TauCoeff/ODPS/ODPS_NC2BIN/run_ODPS_NC2BIN.sh
@@ -0,0 +1,56 @@
+#!/bin/sh -x
+
+#parameters
+#--- the exe file
+EXE_FILE=ODPS_NC2BIN.big_endian
+# Input directory
+#dir_in=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Infrared/netCDF
+#dir_in=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Infrared/ORD-PW/netCDF
+#dir_in=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Infrared/ORD/netCDF
+dir_in=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Microwave/netCDF
+# out directory
+#dir_out=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Infrared/Little_Endian
+#dir_out=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Infrared/ORD-PW/Little_Endian
+#dir_out=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Infrared/ORD/Little_Endian
+dir_out=/state/partition1/home/ychen/trunk/fix/TauCoeff/ODPS/Microwave/Big_Endian
+SENSOR_LIST_FILENAME=sensor_list
+
+mkdir -p ${dir_out}
+
+#--- get the sensor names from the sensor_list file
+SENSOR_LIST=`awk '/BEGIN_LIST/ {
+  while(getline){ 
+    if(match($0,"END_LIST"))exit
+    print $1} 
+}' ${SENSOR_LIST_FILENAME}`
+
+echo ${SENSOR_LIST}
+if [ ! -f ${EXE_FILE} ];then
+  echo "The exe file ${EXE_FILE} does not exist"
+  exit 1
+fi
+
+for SatSen in ${SENSOR_LIST}; do
+
+  #--- work directory for current sensor
+  Sensor_DIR=${dir_in}
+  echo "Processing $Sensor_DIR"
+#  mkdir -p $Sensor_DIR
+  TauCoeffFile=${Sensor_DIR}/${SatSen}.TauCoeff.nc
+  if [ ! -f ${TauCoeffFile} ];then
+    echo "The file ${TauCoeffFile} does not exist"
+    exit 1
+  fi
+#  ln -sf ${TauCoeffFile} ${SatSen}.TauCoeff.nc
+  TauCoeffBIN=${dir_out}/${SatSen}.TauCoeff.bin
+  
+  # run convert ODPS_NC2BIN 
+${EXE_FILE}<<EOF
+${TauCoeffFile}
+${TauCoeffBIN}
+EOF
+ 
+done
+
+exit
+  
