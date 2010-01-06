75f873715e0770b42e492e953e430f04960e73c5
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2010-01-06 17:40:04 +0000

    src/Utility/GSI_Utility/RadDiag/Compute_RadDiag_Stats
      * Compute_RadDiag_Stats.f90: Updated for changes to RadDiag_IO,
      RadDiag_Stats_Define, and RadDiag_Stat_netCDF_IO modules. Replaced
      use of Sensor_Utility module with the SensorInfo linked list
      modules. Not yet tested on current GSI radiance diagnostic files.
      * Makefile: Updated source code list. Now using LINK_SCRIPT and
      UNLINK_SCRIPT macros defined in make.macros include file for
      create_links and remove_links targets.
      * make.dependencies: Regenerated.
    
    
    
    Former-commit-id: 83079dbf6bc25ac1e30452ec1a1226e099cb7739

--- src/Utility/GSI_Utility/RadDiag/Compute_RadDiag_Stats/Compute_RadDiag_Stats.f90
+++ src/Utility/GSI_Utility/RadDiag/Compute_RadDiag_Stats/Compute_RadDiag_Stats.f90
@@ -5,53 +5,62 @@
 PROGRAM Compute_RadDiag_Stats
 
   ! Module usage
-  USE Type_Kinds,              ONLY: sp=>Single
-  USE File_Utility,            ONLY: Get_Lun, File_Exists, Count_Lines_in_File
-  USE Message_Handler,         ONLY: SUCCESS, FAILURE, EOF, &
+  USE Type_Kinds             , ONLY: sp=>Single
+  USE File_Utility           , ONLY: Get_Lun, File_Exists, Count_Lines_in_File
+  USE Message_Handler        , ONLY: SUCCESS, FAILURE, EOF, &
                                      Program_Message, Display_Message
-  USE List_File_Utility,       ONLY: Character_List_File_type, &
+  USE List_File_Utility      , ONLY: Character_List_File_type, &
                                      Read_List_File,           &
                                      Get_List_Size,            &
                                      Get_List_Entry
-  USE Sensor_Utility,          ONLY: Get_SensorAttributes
-  USE SatBias_IO,              ONLY: SatBiasAirMass_type, &
+  USE SensorInfo_Define      , ONLY: SensorInfo_type, &
+                                     Allocate_SensorInfo, &
+                                     Destroy_SensorInfo
+  USE SensorInfo_LinkedList  , ONLY: SensorInfo_List_type, &
+                                     GetFrom_SensorInfo_List, &
+                                     Count_SensorInfo_Nodes, &
+                                     Destroy_SensorInfo_List 
+  USE SensorInfo_IO          , ONLY: Read_SensorInfo
+  USE SatBias_IO             , ONLY: SatBiasAirMass_type, &
                                      MAX_SATBIAS_PREDICTORS, &
                                      Read_SatBias
-  USE RadDiag_IO,              ONLY: RadDiag_Hdr_type,   &
+  USE RadDiag_IO             , ONLY: RadDiag_Hdr_type,   &
                                      RadDiag_Data_type,  &
                                      RADDIAG_READMODE,   &
                                      RADDIAG_WRITEMODE,  &
                                      RADDIAG_APPENDMODE, &
-                                     Open_RadDiag,       &
-                                     Read_RadDiag_Hdr,   &
-                                     Read_RadDiag_Data
-  USE RadDiag_Stats_Define,    ONLY: RadDiag_Stats_type,    &
-                                     invalidFOV,            &
-                                     nVariables,            &
-                                     iBC,                   &
-                                     iNBC,                  &
-                                     iScan,                 &
-                                     iConst,                &
-                                     iAngle,                &
-                                     iLpsR,                 &
-                                     iLpsR2,                &
-                                     iCLW,                  &
-                                     Destroy_RadDiag_Stats, &
-                                     Allocate_RadDiag_Stats
-  USE RadDiag_Stats_netCDF_IO, ONLY: Write_RadDiag_Stats_netCDF
+                                     RadDiag_OpenFile,       &
+                                     RadDiag_Hdr_ReadFile,   &
+                                     RadDiag_Data_ReadFile
+  USE RadDiag_Stats_Define   , ONLY: RadDiag_Stats_type,    &
+                                     INVALID_FOV,           &
+                                     N_VARIABLES,           &
+                                     IBC,                   &
+                                     INBC,                  &
+                                     ISCAN,                 &
+                                     ICONST,                &
+                                     IANGLE,                &
+                                     ILPSR,                 &
+                                     ILPSR2,                &
+                                     ICLW,                  &
+                                     RadDiag_Stats_Destroy, &
+                                     RadDiag_Stats_Create
+  USE RadDiag_Stats_netCDF_IO, ONLY: RadDiag_Stats_WriteFile
 
 
   ! Disable implicit typing
   IMPLICIT NONE
 
   ! Parameters
-  CHARACTER(*), PARAMETER :: PROGRAM_NAME = 'RadDiag_Stats'
-  CHARACTER(*), PARAMETER :: PROGRAM_RCS_ID = &
+  CHARACTER(*), PARAMETER :: PROGRAM_NAME = 'Compute_RadDiag_Stats'
+  CHARACTER(*), PARAMETER :: PROGRAM_VERSION_ID = &
   '$Id$'
-  INTEGER,  PARAMETER :: SET = 1
-  REAL(sp), PARAMETER :: ZERO = 0.0_sp
-  REAL(sp), PARAMETER :: minInverseVariance = 1.0e-06_sp
-  INTEGER,  PARAMETER :: NHEADER = 3
+  
+  CHARACTER(*), PARAMETER :: SENSORINFO_FILE = 'SensorInfo'
+  REAL(sp),     PARAMETER :: ZERO = 0.0_sp                   
+  REAL(sp),     PARAMETER :: minInverseVariance = 1.0e-06_sp 
+  INTEGER,      PARAMETER :: NHEADER = 3                     
+  INTEGER,      PARAMETER :: SET = 1
 
 
   ! Variables
@@ -65,6 +74,9 @@ PROGRAM Compute_RadDiag_Stats
   INTEGER :: FileID
   INTEGER :: Error_Status
   INTEGER :: Read_Status
+  TYPE(SensorInfo_type) :: SensorInfo      
+  TYPE(SensorInfo_List_type) :: SensorInfo_List
+  INTEGER :: n_Sensors  
   TYPE(Character_List_File_type) :: RadDiag_TimeList
   CHARACTER(200), DIMENSION(NHEADER), TARGET :: RadDiag_TimeList_Hdr
   CHARACTER(200), POINTER :: Sensor_Id             => NULL()
@@ -76,14 +88,11 @@ PROGRAM Compute_RadDiag_Stats
   TYPE(RadDiag_Stats_type) :: RadDiag_Stats
   INTEGER, DIMENSION(:), ALLOCATABLE :: MatchIndex
   INTEGER :: i, j, k, m, n
-  INTEGER :: nLines
-  INTEGER :: iTime, nTimes
-  INTEGER :: nFOVs
+  INTEGER :: n_Lines
+  INTEGER :: iTime, n_Times
+  INTEGER :: n_FOVs
   CHARACTER(10) :: TimeStamp
-  CHARACTER(80) :: Sensor_Name
-  CHARACTER(80) :: Platform_Name
   REAL(sp) :: rnSamples
-
 
 
   ! Output a program header message
@@ -92,35 +101,56 @@ PROGRAM Compute_RadDiag_Stats
                         'compute various statistics over scan position and time.', &
                         '$Revision$' )
 
+
+  ! Read the SensorInfo file
+  IF ( .NOT. File_Exists(SENSORINFO_FILE) ) THEN
+    CALL Display_Message(PROGRAM_NAME, 'SensorInfo file not found', FAILURE)
+    STOP
+  END IF
+  Error_Status = Read_SensorInfo( SENSORINFO_FILE,SensorInfo_List,Quiet=1 )
+  IF ( Error_Status /= SUCCESS ) THEN
+    CALL Display_Message(PROGRAM_NAME, 'Error reading SensorInfo file '//SENSORINFO_FILE, FAILURE)
+    STOP
+  END IF
+  n_Sensors = Count_SensorInfo_Nodes( SensorInfo_List )
+  IF ( n_Sensors < 1 ) THEN
+    CALL Display_Message( PROGRAM_NAME, 'SensorInfo_List is empty.', FAILURE )
+    STOP
+  END IF
+  
+  
   ! Get the RadDiag List filename
-  WRITE( *, FMT = '( /5x, "Enter a RadDiag time list filename: " )', ADVANCE = 'NO' )
-  READ( *, '( a )' ) TimeList_Filename
+  WRITE( *,FMT='(/5x,"Enter a RadDiag time list filename: ")', ADVANCE='NO' )
+  READ( *,'(a)' ) TimeList_Filename
   TimeList_Filename = ADJUSTL(TimeList_Filename)
 
+
   ! Read the RadDiag list file
-  Error_Status = Read_List_File( TimeList_Filename, &
-                                 RadDiag_TimeList )
+  Error_Status = Read_List_File( TimeList_Filename, RadDiag_TimeList )
   IF ( Error_Status /= SUCCESS ) THEN
-    CALL Display_Message(PROGRAM_NAME, 'Error reading list file '//TRIM(TimeList_Filename), Error_Status)
+    CALL Display_Message(PROGRAM_NAME, 'Error reading list file '//TRIM(TimeList_Filename), FAILURE)
     STOP
   END IF
 
+
   ! Get the RadDiag_Stats output filename
-  WRITE( *, FMT = '( /5x, "Enter a RadDiag_Stats output filename: " )', ADVANCE = 'NO' )
-  READ( *, '( a )' ) Output_Filename
+  WRITE( *,FMT='(/5x,"Enter a RadDiag_Stats output filename: ")', ADVANCE='NO' )
+  READ( *,'(a)' ) Output_Filename
   Output_Filename = ADJUSTL(Output_Filename)
 
+
   ! Get an output file comment
-  WRITE( *, '( /5x, "Enter Comment string for output file:" )' )
-  READ( *, '( a )' ) Comment
+  WRITE( *,'(/5x,"Enter comment string for output file:")' )
+  READ( *,'(a)' ) Comment
   Comment = ADJUSTL(Comment)
+
 
   ! Determine the number of list time entries
   ! - First entry is the sensor name id,
   ! - Second is the RadDiag file prefix,
   ! - Third is the air mass bias correction file prefix
   ! hence the -3
-  nTimes = Get_List_Size( RadDiag_TimeList ) - NHEADER
+  n_Times = Get_List_Size( RadDiag_TimeList ) - NHEADER
 
   ! Read the time list header
   DO i = 1, NHEADER
@@ -142,15 +172,32 @@ PROGRAM Compute_RadDiag_Stats
   RadDiag_Prefix        => RadDiag_TimeList_Hdr(2)
   SatBiasAirMass_Prefix => RadDiag_TimeList_Hdr(3)
 
-  ! Get the sensor attributes
-  CALL Get_SensorAttributes(Sensor_Id, &
-                            nFOVs=nFOVs, &
-                            Sensor_Name=Sensor_Name, &
-                            Platform_Name=Platform_Name)
+
+  ! Search the SensorInfo list for the required sensor attributes
+  n_FOVs = -1
+  Sensor_Loop: DO n = 1, n_Sensors
+    ! Get the current SensorInfo node from the list
+    Error_Status = GetFrom_SensorInfo_List( SensorInfo_List,n,SensorInfo )
+    IF ( Error_Status /= SUCCESS ) THEN
+      Error_Status = FAILURE
+      WRITE( Message,'("Error retrieving SensorInfo data for sensor # ",i0)' ) n
+      CALL Display_Message( PROGRAM_NAME,Message,Error_Status )
+      STOP
+    END IF
+    ! Is it the required sensor?
+    IF ( TRIM(Sensor_Id) == TRIM(SensorInfo%Sensor_Id) ) EXIT Sensor_Loop
+  END DO Sensor_Loop
+  ! Check result
+  IF ( TRIM(Sensor_Id) /= TRIM(SensorInfo%Sensor_Id) ) THEN
+    CALL Display_Message( PROGRAM_NAME,TRIM(Sensor_Id)//' sensor not found!', FAILURE )
+    STOP
+  END IF
+
+  
 
   ! Loop over RadDiag times
   m = 0
-  File_Loop: DO iTime = 1, nTimes
+  File_Loop: DO iTime = 1, n_Times
 
     ! Get the current RadDiag filename
     Error_Status = Get_List_Entry( RadDiag_TimeList, &
@@ -177,14 +224,14 @@ PROGRAM Compute_RadDiag_Stats
     m = m + 1
 
     ! Count the number of lines(channels) in the SatBias AirMass file
-    nLines = Count_Lines_in_File( SatBiasAirMass_Filename, NoComment='!', NoBlank=SET )
-    IF ( nLines == 0 ) THEN
+    n_Lines = Count_Lines_in_File( SatBiasAirMass_Filename, NoComment='!', NoBlank=SET )
+    IF ( n_Lines == 0 ) THEN
       CALL Display_Message(PROGRAM_NAME, 'Error counting lines in '//TRIM(SatBiasAirMass_Filename), FAILURE)
       STOP
     END IF
     
     ! Open the RadDiag file
-    Error_Status = Open_RadDiag( RadDiag_Filename, FileID )
+    Error_Status = RadDiag_OpenFile( RadDiag_Filename, FileID )
     IF ( Error_Status /= SUCCESS ) THEN
       CALL Display_Message(PROGRAM_NAME, 'Error opening '//TRIM(RadDiag_Filename), FAILURE)
       STOP
@@ -192,7 +239,7 @@ PROGRAM Compute_RadDiag_Stats
 
     ! Read the RadDiag file header
     WRITE(*, '( /5x, "Reading ", a, " header...." )' ) TRIM( RadDiag_Filename )
-    Error_Status = Read_RadDiag_Hdr( RadDiag_Filename, FileID, RadDiag_Hdr )
+    Error_Status = RadDiag_Hdr_ReadFile( FileID, RadDiag_Hdr )
     IF ( Error_Status /= SUCCESS ) THEN
       CALL Display_Message(PROGRAM_NAME, 'Error reading header from '//TRIM(RadDiag_Filename), FAILURE)
       STOP
@@ -200,23 +247,26 @@ PROGRAM Compute_RadDiag_Stats
 
     ! Perform the various allocations
     IF ( iTime == 1 ) THEN
-      ALLOCATE( SatBiasAirMass( nLines ), MatchIndex(RadDiag_Hdr%nChannels), STAT=Error_Status )
+      ALLOCATE( SatBiasAirMass( n_Lines ), MatchIndex(RadDiag_Hdr%n_Channels), STAT=Error_Status )
       IF ( Error_Status /= 0 ) THEN
         CALL Display_Message(PROGRAM_NAME, 'Error allocating SatBiasAirMass structure array', FAILURE)
         STOP
       END IF
-      Error_Status = Allocate_RadDiag_Stats( MAX_SATBIAS_PREDICTORS, &
-                                             RadDiag_Hdr%nChannels, &
-                                             nFOVs, &
-                                             nTimes, &
-                                             nVariables, &
-                                             RadDiag_Stats)
+      CALL RadDiag_Stats_Create( RadDiag_Stats, &
+                                 MAX_SATBIAS_PREDICTORS, &
+                                 RadDiag_Hdr%n_Channels, &
+                                 SensorInfo%n_FOVs, &
+                                 n_Times )
       IF ( Error_Status /= SUCCESS ) THEN
         CALL Display_Message(PROGRAM_NAME, 'Error allocating RadDiag_Stats arrays', FAILURE)
         STOP
       END IF
       ! Save the channel numbers
       RadDiag_Stats%Channel = RadDiag_Hdr%Channel(:)%nuchan
+      ! Fill the Sensor Id values
+      RadDiag_Stats%Sensor_Id        = SensorInfo%Sensor_Id
+      RadDiag_Stats%WMO_Satellite_ID = SensorInfo%WMO_Satellite_ID
+      RadDiag_Stats%WMO_Sensor_ID    = SensorInfo%WMO_Sensor_ID
     END IF
 
     ! Save the current date/time
@@ -234,12 +284,12 @@ PROGRAM Compute_RadDiag_Stats
 
     ! Assign the SatBias AirMass data into the RadDiag_Stats structure
     n = COUNT( SatBiasAirMass%Sensor_Id == Sensor_Id )
-    IF ( n /= RadDiag_Hdr%nChannels ) THEN
+    IF ( n /= RadDiag_Hdr%n_Channels ) THEN
       CALL Display_Message(PROGRAM_NAME, 'Channel count mismatch in SatBias entries', FAILURE)
       STOP
     END IF
-    MatchIndex = PACK( (/ (n,n=1,nLines) /), SatBiasAirMass%Sensor_Id == Sensor_Id )
-    DO n=1,RadDiag_Hdr%nChannels
+    MatchIndex = PACK( (/ (n,n=1,n_Lines) /), SatBiasAirMass%Sensor_Id == Sensor_Id )
+    DO n=1,RadDiag_Hdr%n_Channels
       RadDiag_Stats%AirMassCoefficients(:,n,iTime) = SatBiasAirMass(MatchIndex(n))%c
     END DO
 
@@ -252,13 +302,13 @@ PROGRAM Compute_RadDiag_Stats
     Read_Loop: DO
 
       ! Read the current entry
-      Read_Status = Read_RadDiag_Data( RadDiag_Filename, FileID, RadDiag_Hdr, RadDiag_Data )
+      Read_Status = RadDiag_Data_ReadFile( FileID, RadDiag_Hdr, RadDiag_Data )
       SELECT CASE (Read_Status)
         CASE (EOF)
           EXIT Read_Loop
         CASE (FAILURE)
           CALL Display_Message(PROGRAM_NAME, 'Error reading data from '//TRIM( RadDiag_Filename ), FAILURE)
-          STOP
+          EXIT Read_Loop !STOP
         CASE DEFAULT
           ! Success or warning: do nothing
       END SELECT
@@ -271,7 +321,7 @@ PROGRAM Compute_RadDiag_Stats
       RadDiag_Stats%FOV(k) = k
 
       ! Loop over channels to sum data
-      Channel_Loop: DO j = 1, RadDiag_Data%nChannels
+      Channel_Loop: DO j = 1, RadDiag_Data%n_Channels
         IF ( RadDiag_Data%Channel(j)%errinv > minInverseVariance ) THEN
 
           ! Scan position summation
@@ -283,7 +333,7 @@ PROGRAM Compute_RadDiag_Stats
           RadDiag_Stats%scan_Data(iLpsR ,j,k) = RadDiag_Stats%scan_Data(iLpsR ,j,k) + RadDiag_Data%Channel(j)%bilap
           RadDiag_Stats%scan_Data(iLpsR2,j,k) = RadDiag_Stats%scan_Data(iLpsR2,j,k) + RadDiag_Data%Channel(j)%bilap2
           RadDiag_Stats%scan_Data(iCLW  ,j,k) = RadDiag_Stats%scan_Data(iCLW  ,j,k) + RadDiag_Data%Channel(j)%biclw
-          RadDiag_Stats%scan_nSamples(j,k)       = RadDiag_Stats%scan_nSamples(j,k) + 1
+          RadDiag_Stats%scan_nSamples(j,k)    = RadDiag_Stats%scan_nSamples(j,k) + 1
 
           ! Timeseries summation
           RadDiag_Stats%time_Data(iBC,j,m)    = RadDiag_Stats%time_Data(iBC,j,m)    + RadDiag_Data%Channel(j)%omgbc 
@@ -304,19 +354,19 @@ PROGRAM Compute_RadDiag_Stats
 
   ! Replace the RadDiag_Stats nTimes dimension with
   ! the actual number of times/files read
-  RadDiag_Stats%nTimes = m
+  RadDiag_Stats%n_Times = m
 
   ! Compute the scan averages
   WRITE(*, '( /5x, "Computing scan averages...." )' )
-  FOV_Loop: DO k = 1, RadDiag_Stats%nFOVS
+  FOV_Loop: DO k = 1, RadDiag_Stats%n_FOVS
 
     ! If FOV not used, go to next one
-    IF ( RadDiag_Stats%FOV(k) == invalidFOV ) CYCLE FOV_Loop
+    IF ( RadDiag_Stats%FOV(k) == INVALID_FOV ) CYCLE FOV_Loop
 
     ! Loop over channels to compute averages
-    DO j = 1, RadDiag_Stats%nChannels
+    DO j = 1, RadDiag_Stats%n_Channels
       rnSamples = REAL(RadDiag_Stats%scan_nSamples(j,k),sp)
-      DO i = 1, RadDiag_Stats%nVariables
+      DO i = 1, RadDiag_Stats%n_Variables
         RadDiag_Stats%scan_Data(i,j,k) = RadDiag_Stats%scan_Data(i,j,k)/rnSamples
       END DO
     END DO
@@ -325,12 +375,12 @@ PROGRAM Compute_RadDiag_Stats
 
   ! Compute the timeseries averages
   WRITE(*, '( /5x, "Computing timeseries averages...." )' )
-  DateTime_Loop: DO m = 1, RadDiag_Stats%nTimes
+  DateTime_Loop: DO m = 1, RadDiag_Stats%n_Times
 
     ! Loop over channels to compute averages
-    DO j = 1, RadDiag_Stats%nChannels
+    DO j = 1, RadDiag_Stats%n_Channels
       rnSamples = REAL(RadDiag_Stats%time_nSamples(j,m),sp)
-      DO i = 1, RadDiag_Stats%nVariables
+      DO i = 1, RadDiag_Stats%n_Variables
         RadDiag_Stats%time_Data(i,j,m) = RadDiag_Stats%time_Data(i,j,m)/rnSamples
       END DO
     END DO
@@ -341,28 +391,24 @@ PROGRAM Compute_RadDiag_Stats
   WRITE(Title,'("RadDiag Stats for ", a, " from ", i10, " to ", i10 )' ) &
               TRIM(Sensor_Id), &
               RadDiag_Stats%DateTime(1), &
-              RadDiag_Stats%DateTime(RadDiag_Stats%nTimes)
+              RadDiag_Stats%DateTime(RadDiag_Stats%n_Times)
   WRITE(*, '( /5x, "Writing output file ", a, "...." )' ) TRIM(Output_Filename)
   
-  Error_Status = Write_RadDiag_Stats_netCDF( TRIM(Output_Filename), &
-                                             RadDiag_Stats, &
-                                             Title        =TRIM(Title), &
-                                             History      =PROGRAM_RCS_ID, &
-                                             Sensor_Name  =TRIM(Sensor_Name), &
-                                             Platform_Name=TRIM(Platform_Name), &
-                                             Comment      =TRIM(Comment) )
+  Error_Status = RadDiag_Stats_WriteFile( TRIM(Output_Filename), &
+                                          RadDiag_Stats, &
+                                          Title   = TRIM(Title), &
+                                          History = PROGRAM_VERSION_ID, &
+                                          Comment = TRIM(Comment) )
   IF ( Error_Status /= SUCCESS ) THEN
     CALL Display_Message(PROGRAM_NAME, 'Error writing output file '//TRIM(Output_Filename), Error_Status)
     STOP
   END IF
 
 
-  ! Deallocate the arrays
+  ! Cleanup
   DEALLOCATE( SatBiasAirMass, MatchIndex )
-  Error_Status = Destroy_RadDiag_Stats( RadDiag_Stats )
-  IF ( Error_Status /= SUCCESS ) THEN
-    CALL Display_Message(PROGRAM_NAME, 'Error deallocating RadDiag_Stats structure', Error_Status)
-    STOP
-  END IF
+  CALL RadDiag_Stats_Destroy( RadDiag_Stats )
+  Error_Status = Destroy_SensorInfo( SensorInfo )
+  Error_Status = Destroy_SensorInfo_List( SensorInfo_List )
 
 END PROGRAM Compute_RadDiag_Stats
