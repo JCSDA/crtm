1c0449c0784d124c699f4030c8efcefbfa08a66a
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2011-04-25 21:19:20 +0000

    src/Utility/FFT_Utility/Prime_Utility/Test   * Test_Prime_Utility.f90: Added capability for user to specify   the number for which prime factors are required.   * Makefile: Made targets conform to the GNU double-colon syntax.   Corrected linking script invocations to use macros.
    Former-commit-id: 6a4c646e784e93887178be2e8844eb6b0db194eb

--- src/Utility/FFT_Utility/Prime_Utility/Test/Test_Prime_Utility.f90
+++ src/Utility/FFT_Utility/Prime_Utility/Test/Test_Prime_Utility.f90
@@ -5,7 +5,7 @@ PROGRAM Test_Prime_Utility
   INTEGER, PARAMETER :: NTEST=500
   INTEGER, PARAMETER :: NPFTEST=9438
   ! Variables
-  INTEGER :: i, j, errStatus
+  INTEGER :: i, j, errStatus, test_number
   INTEGER :: prod
   TYPE(Prime_type) :: Prime
 
@@ -25,14 +25,29 @@ PROGRAM Test_Prime_Utility
   errStatus = Destroy_Prime(Prime)
 
   Prime=PrimeFactor(NPFTEST)
-  WRITE(*,'(//5x,"Prime factors for ",i0,":")') Prime%number
-  prod=1
-  DO i=1,Prime%nPrimes
-    prod=prod*(Prime%n(i)**Prime%nExp(i))
-    WRITE(*,'(i0,"^",i0)',ADVANCE='NO') Prime%n(i), Prime%nExp(i)
-    IF(i < Prime%nPrimes) WRITE(*,'(" x ")',ADVANCE='NO')
-  END DO
-  WRITE(*,'(" = ",i0)')prod
+  CALL Display_Primes()
   errStatus = Destroy_Prime(Prime)
+  
+  Test_Loop: DO
+    WRITE( *,FMT='(//5x,"Enter integer number to test [-ve to quit]: ")',ADVANCE='NO' )
+    READ(*,*) test_number
+    IF ( test_number < 0 ) EXIT Test_Loop
+    Prime=PrimeFactor(test_number)
+    CALL Display_Primes()
+    errStatus = Destroy_Prime(Prime)
+  END DO Test_Loop
+  
+CONTAINS
+
+  SUBROUTINE Display_Primes()
+    WRITE(*,'(/5x,"Prime factors for ",i0,":")') Prime%number
+    prod=1
+    DO i=1,Prime%nPrimes
+      prod=prod*(Prime%n(i)**Prime%nExp(i))
+      WRITE(*,'(i0,"^",i0)',ADVANCE='NO') Prime%n(i), Prime%nExp(i)
+      IF(i < Prime%nPrimes) WRITE(*,'(" x ")',ADVANCE='NO')
+    END DO
+    WRITE(*,'(" = ",i0)')prod
+  END SUBROUTINE Display_Primes
   
 END PROGRAM Test_Prime_Utility
