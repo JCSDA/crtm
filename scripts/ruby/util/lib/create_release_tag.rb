417ee855d178d0078e82d8ca99f9565aa4d50c4a
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2011-09-08 15:52:06 +0000

    Script to create a CRTM release tag.   See CRTM Trac Ticket #163
      scripts/ruby/util/lib
      * create_release_tag.rb: Added parameter info identifying fixfiles.
      Added the creation of the fixfile directories in the release path.
      Added svn copy of the fixfile directories from the tag path to the
      release path.
    
    
    
    Former-commit-id: 6df9b803b7c45aaa8b1e707e5c0328f4f75d9744

--- scripts/ruby/util/lib/create_release_tag.rb
+++ scripts/ruby/util/lib/create_release_tag.rb
@@ -30,14 +30,26 @@ require 'fileutils'
 require 'svn'
 require 'inventory'
 
+
 # Specify parameters
-CRTM_ENV = [{:name => "CRTM_SOURCE_ROOT"    ,:dir => "/src"},
-            {:name => "CRTM_FIXFILE_ROOT"   ,:dir => "/fix"},
-            {:name => "CRTM_TEST_ROOT"      ,:dir => "/test"},
-            {:name => "CRTM_EXTERNALS_ROOT" ,:dir => "/externals"},
-            {:name => "CRTM_SCRIPTS_ROOT"   ,:dir => "/scripts"},
-            {:name => "CRTM_DOC_ROOT"       ,:dir => "/doc"},
+# ...CRTM environment parameters
+CRTM_ENV = [{:name => "CRTM_SOURCE_ROOT"    ,:dir => "/src"       },
+            {:name => "CRTM_FIXFILE_ROOT"   ,:dir => "/fix"       },
+            {:name => "CRTM_TEST_ROOT"      ,:dir => "/test"      },
+            {:name => "CRTM_EXTERNALS_ROOT" ,:dir => "/externals" },
+            {:name => "CRTM_SCRIPTS_ROOT"   ,:dir => "/scripts"   },
+            {:name => "CRTM_DOC_ROOT"       ,:dir => "/doc"       },
             {:name => "CRTM_VALIDATION_ROOT",:dir => "/validation"}]
+# ...Fixfile information
+FIXFILE_DIRS = ["TauCoeff/ODAS"                ,
+                "TauCoeff/ODPS"                ,
+                "SpcCoeff"                     ,
+                "AerosolCoeff"                 ,
+                "CloudCoeff"                   ,
+                "EmisCoeff/IR_Land/LSEcategory",
+                "EmisCoeff/IR_Water"           ]
+FIXFILE_FORMAT = ["Big_Endian","Little_Endian"]
+# ...Inventory file for the tag->release transfer
 INVENTORY_FILE = "inventory.txt"
 
 
@@ -57,11 +69,9 @@ options[:commit] = true
 # ...Specify the options
 opts = OptionParser.new do |opts|
   opts.banner = "Usage: create_release_tag.rb [options] tag_wcpath release_wcpath"
-
   opts.on("-n", "--no-commit", "create release, but do not commit to the repository") do |n|
     options[:commit] = n
   end
-  
   opts.on_tail("-h", "--help", "Show this message") do
     puts opts
     exit(0)
@@ -87,23 +97,24 @@ begin
   svn = Svn_Util::Svn.new()
   puts svn.versioned?(release_wcpath)
 
-# TEMPORARY
-if File.directory?(release_wcpath)
-  system("svn revert --depth infinity #{release_wcpath}")
-  FileUtils.rm_rf(release_wcpath)
-end
-# TEMPORARY
 
-
+  # Create the release working copy
   raise "#{release_wcpath} already exists!" if File.directory?(release_wcpath)
   raise "Error creating #{release_wcpath}" unless svn.mkdir(release_wcpath)
+  # ...Create subdirectories for fixfiles
+  FIXFILE_DIRS.each do |fdir|
+    raise "Error creating #{fdir} fixfile directory" unless svn.mkdir("#{release_wcpath}/fix/#{fdir}",:parents => true)
+  end
 
+  # Specify the destination directory for the subversion commands
   dest = FileUtils.pwd()+"/#{release_wcpath}"
-  
+
+  # Enter the existing tag working copy
   FileUtils.cd(tag_wcpath) do
     raise "#{tag_wcpath} is not a Subversion working copy!" unless File.directory?(".svn")
     raise "#{tag_wcpath} is not a CRTM root directory!" unless File.exist?("Set_CRTM_Environment.sh")
     set_crtm_environment()
+    # Move over the source tree from tag->release  
     FileUtils.cd("src") do
       system("make create_links")
       FileUtils.cd("Build") do
@@ -112,9 +123,18 @@ end
         inventory.linkto.each {|k,v| svn.copy(v,"#{dest}/#{k}",:parents => true)}
       end
     end
+    # Move over the fix tree from tag->release
+    FileUtils.cd("fix") do
+      FIXFILE_DIRS.each do |fdir|
+        FIXFILE_FORMAT.each do |ffmt|
+          svn.copy("#{fdir}/#{ffmt}","#{dest}/fix/#{fdir}",:parents => true)
+        end
+      end
+    end
   end
   
-  svn.commit(release_wcpath,:message => "Committing release #{release_wcpath}") if options[:commit]
+  # Commit the release
+  svn.commit(release_wcpath,:message => "#{release_wcpath} release") if options[:commit]
 
 
 # Inform user why script barfed
