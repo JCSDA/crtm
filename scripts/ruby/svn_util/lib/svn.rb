db0e640ca844c7a666390c0a89714778a6cfba20
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2011-10-25 17:43:53 +0000

    Support subversion module for script to create a   release of the CRTM.   See CRTM Trac Ticket #163   https://svnemc.ncep.noaa.gov/trac/crtm/ticket/163
      scripts/ruby/svn_util/lib
      * svn.rb: Rewrite of module to allow easier specification
      of the subversion subcommand options, thus making it easier
      to add additional commands in the future.
    
    
    
    Former-commit-id: bd6c86975dec8d78c0baf0b41001f105503df76a

--- scripts/ruby/svn_util/lib/svn.rb
+++ scripts/ruby/svn_util/lib/svn.rb
@@ -1,200 +1,412 @@
-# Put svn_util heirarchy at
-# beginning of search path
+# Put hierarchy at beginning of search path
 $:.unshift File.join(File.dirname(__FILE__))
 
+# Module containing equivalent subversion commands for use
+# in ruby scripts. Note that the structure of, and any associated
+# smart ideas within, this module were unceremoniously cribbed
+# from the fileutils module.
+
 module Svn_Util
-
-  require 'xmlsimple'
-  require 'yaml'
-
+  require 'rexml/document'
+  
   class Svn
+  
+    # Hash tables to hold command options.
+    OPT_TABLE = {}   #:nodoc:
 
-    # The subversion actions associated
-    # with particular file statuses
-    ACTION = { "?" => "add",
-               "!" => "remove" }
+
+    attr_accessor :noop
     
-    # Make some of the instance variables visible
-    attr_reader :url, :path
-    
-    # Public methods
-    # --------------
-    def initialize()
-      @url    = nil
-      @path   = nil
-      @info   = nil
-      @status = {}
-      @list   = {}
+    def initialize(noop = false)
+      @noop = noop
+      @history = []
     end
+  
+    
 
-
-    def checkout(url,path="",options={})
-      ignore_externals = options[:ignore_externals] ? "--ignore-externals " : ""
-      quiet            = options[:quiet]            ? "--quiet " : ""
-      depth            = options[:depth]            ? "--depth #{options[:depth]} " : ""
-      revision         = options[:revision]         ? "--revision #{options[:revision]} " : ""
-      @url = url
-      cmd = "svn checkout #{ignore_externals}#{quiet}#{depth}#{revision} #{url} #{path}"
-      system(cmd)
+    # -------------------------------------------------------
+    # Methods implementing the various subversion subcommands
+    # -------------------------------------------------------
+    
+    #
+    # add: Add files, directories, or symbolic links.
+    #
+    # Calling sequence: add(list,options={})
+    #
+    # Options: auto_props
+    #          depth ARG
+    #          force
+    #          no_auto_props
+    #          no_ignore
+    #          parents
+    #          quiet
+    #          targets FILENAME
+    #
+    def add(list,options={})
+      svn_check_options(options, OPT_TABLE['add'])
+      option_list = svn_process_options(options)
+      cmd = "svn add #{option_list} #{list}"
+      svn_run_command(cmd)
     end
-    
-    
+  
+    OPT_TABLE['add'] = [:auto_props, :depth, :force, :no_auto_props, :no_ignore,
+                        :parents, :quiet, :targets]
+
+
+    #
+    # checkout: Check out a working copy from a repository
+    #
+    # Calling sequence: checkout(url,path,options={})
+    #
+    # Aliases: co
+    #
+    # Options: depth ARG
+    #          force
+    #          ignore_externals
+    #          quiet
+    #          revision REV
+    #
+    def checkout(url,path,options={})
+      svn_check_options(options, OPT_TABLE['checkout'])
+      option_list = svn_process_options(options)
+      cmd = "svn checkout #{option_list} #{url} #{path}"
+      svn_run_command(cmd)
+    end
+  
+    alias co checkout
+  
+    OPT_TABLE['checkout'] =
+    OPT_TABLE['co']       = [:depth, :force, :ignore_externals, :quiet, :revision]
+
+
+    #
+    # commit: Send changes from your working copy to the repository
+    #
+    # Calling sequence: commit(path,options={})
+    #
+    # Aliases: ci
+    #
+    # Options: depth ARG
+    #          editor_cmd CMD
+    #          encoding ENC
+    #          file FILENAME
+    #          force_log
+    #          keep_changelists
+    #          message MESSAGE
+    #          no_unlock
+    #          quiet
+    #          targets FILENAME
+    #          with_revprop ARG
+    #
     def commit(path,options={})
-      if options[:message] then
-        cmd = "svn commit --message \"#{options[:message]}\" #{path}"
-      end
-      if options[:file] then
-        cmd = "svn commit --file #{options[:message]} #{path}"
-      end
-      system(cmd)
+      svn_check_options(options, OPT_TABLE['commit'])
+      option_list = svn_process_options(options)
+      cmd = "svn commit #{option_list} #{path}"
+      svn_run_command(cmd)
     end
-    
-    
+  
+    alias ci commit
+  
+    OPT_TABLE['commit'] = 
+    OPT_TABLE['ci']     = [:depth, :editor_cmd, :encoding, :file, :force_log,
+                           :keep_changelists, :message, :no_unlock, :quiet,
+                           :targets, :with_revprop]
+
+
+    #
+    # copy: Copy a file or directory in a working copy or in the repository.
+    #
+    # Calling sequence: copy(source,destination,options={})
+    #
+    # Aliases: cp
+    #
+    # Options: editor_cmd CMD
+    #          encoding ENC
+    #          file FILENAME
+    #          force_log
+    #          ignore_externals
+    #          message MESSAGE
+    #          parents
+    #          quiet
+    #          revision REV
+    #          with_revprop ARG
+    #
     def copy(source,destination,options={})
-      peg_revision     = options[:peg_revision]     ? "@#{options[:peg_revision]}" : ""
-      file             = options[:file]             ? "--file #{options[:file]} " : ""
-      force_log        = options[:force_log]        ? "--force-log " : ""
-      ignore_externals = options[:ignore_externals] ? "--ignore-externals " : ""
-      message          = options[:message]          ? "--message #{options[:message]} " : ""
-      parents          = options[:parents]          ? "--parents " : ""
-      quiet            = options[:quiet]            ? "--quiet " : ""
-      revision         = options[:revision]         ? "--revision #{options[:revision]} " : ""
-      with_revprop     = options[:with_revprop]     ? "--with-revprop #{options[:with_revprop]} " : ""
-      cmd = "svn copy #{file}#{force_log}#{ignore_externals}#{message}#{parents}#{quiet}#{revision}#{with_revprop} #{source}#{peg_revision} #{destination}"
-      system(cmd)
+      svn_check_options(options, OPT_TABLE['copy'])
+      option_list = svn_process_options(options)
+      cmd = "svn copy #{option_list} #{source} #{destination}"
+      svn_run_command(cmd)
     end
-    
-    
-    def mkdir(directory,options={})
-      file             = options[:file]             ? "--file #{options[:file]} " : ""
-      force_log        = options[:force_log]        ? "--force-log " : ""
-      message          = options[:message]          ? "--message #{options[:message]} " : ""
-      parents          = options[:parents]          ? "--parents " : ""
-      quiet            = options[:quiet]            ? "--quiet " : ""
-      with_revprop     = options[:with_revprop]     ? "--with-revprop #{options[:with_revprop]} " : ""
-      cmd = "svn mkdir #{file}#{force_log}#{message}#{parents}#{quiet}#{with_revprop} #{directory}"
-      system(cmd)
+  
+    alias cp copy
+  
+    OPT_TABLE['copy'] = 
+    OPT_TABLE['cp']   = [:editor_cmd, :encoding, :file, :force_log, :ignore_externals,
+                         :message, :parents, :quiet, :revision, :with_revprop]
+
+
+    #
+    # delete: Delete an item from a working copy or the repository.
+    #
+    # Calling sequence: delete(list,options={})
+    #
+    # Aliases: del, remove, rm
+    #
+    # Options: editor_cmd CMD
+    #          encoding ENC
+    #          file FILENAME
+    #          force
+    #          force_log
+    #          keep_local
+    #          message MESSAGE
+    #          quiet
+    #          targets FILENAME
+    #          with_revprop ARG
+    #
+    def delete(list,options={})
+      svn_check_options(options, OPT_TABLE['delete'])
+      option_list = svn_process_options(options)
+      cmd = "svn delete #{option_list} #{list}"
+      svn_run_command(cmd)
     end
+  
+    alias del    delete
+    alias remove delete
+    alias rm     delete
+  
+    OPT_TABLE['delete'] = 
+    OPT_TABLE['del']    = 
+    OPT_TABLE['remove'] = 
+    OPT_TABLE['rm']     = [:editor_cmd, :encoding, :file, :force, :force_log, :keep_local,
+                           :message, :quiet, :targets, :with_revprop]
+
+
+    #
+    # info: Display information about a local or remote item.
+    #
+    # Calling sequence: info(list,options={})
+    #
+    # Options: changelist ARG
+    #          depth ARG
+    #          incremental
+    #          recursive
+    #          revision REV
+    #          targets FILENAME
+    #          xml
+    #
+    def info(list,options={})
+      svn_check_options(options, OPT_TABLE['info'])
+      option_list = svn_process_options(options)
+      cmd = "svn info #{option_list} #{list}"
+      svn_run_command(cmd)
+    end
+  
+    OPT_TABLE['info'] = [:changelist, :depth, :incremental, :recusrsive, :revision, 
+                         :targets, :xml]
+
+
+    #
+    # log: Display commit log messages.
+    #
+    # Calling sequence: log(path,options={})
+    #
+    # Options: change ARG
+    #          incremental
+    #          limit NUM
+    #          quiet
+    #          revision REV
+    #          stop_on_copy
+    #          targets FILENAME
+    #          use_merge_history
+    #          verbose
+    #          with_all_revprops
+    #          with_no_revprops
+    #          with_revprop ARG
+    #          xml
+    #
+    def log(path,options={})
+      svn_check_options(options, OPT_TABLE['log'])
+      option_list = svn_process_options(options)
+      cmd = "svn log #{option_list} #{path}"
+      svn_run_command(cmd)
+    end
+  
+    OPT_TABLE['log'] = [:change, :incremental, :limit, :quiet, :revision,
+                        :stop_on_copy, :targets, :use_merge_history, :verbose,
+                        :with_all_revprops, :with_no_revprops, :with_revprop, :xml]
+
+
+    #
+    # revert: Undo all local edits.
+    #
+    # Calling sequence: revert(path,options={})
+    #
+    # Options: changelist ARG
+    #          depth ARG
+    #          quiet
+    #          recursive
+    #          targets FILENAME
+    #
+    def revert(path,options={})
+      svn_check_options(options, OPT_TABLE['revert'])
+      option_list = svn_process_options(options)
+      cmd = "svn revert #{option_list} #{path}"
+      svn_run_command(cmd)
+    end
+  
+    OPT_TABLE['revert'] = [:changelist, :depth, :quiet, :recursive, :targets]
+
+
+    #
+    # mkdir: Create a new directory under version control.
+    #
+    # Calling sequence: mkdir(new,options={})
+    #
+    # Options: editor_cmd CMD
+    #          encoding ENC
+    #          file FILENAME
+    #          force_log
+    #          message MESSAGE
+    #          parents
+    #          quiet
+    #          with_revprop ARG
+    #
+    def mkdir(new,options={})
+      svn_check_options(options, OPT_TABLE['mkdir'])
+      option_list = svn_process_options(options)
+      cmd = "svn mkdir #{option_list} #{new}"
+      svn_run_command(cmd)
+    end
+  
+    OPT_TABLE['mkdir'] = [:editor_cmd, :encoding, :file, :force_log, :message,
+                          :parents, :quiet, :with_revprop]
+
+
+    #
+    # status: Print the status of working copy files and directories.
+    #
+    # Calling sequence: status(path,options={})
+    #
+    # Aliases: stat, st
+    #
+    # Options: changelist ARG
+    #          depth ARG
+    #          ignore_externals
+    #          incremental
+    #          no_ignore
+    #          quiet
+    #          show_updates
+    #          verbose
+    #          xml
+    #
+    def status(path,options={})
+      svn_check_options(options, OPT_TABLE['status'])
+      option_list = svn_process_options(options)
+      cmd = "svn status #{option_list} #{path}"
+      svn_run_command(cmd)
+    end
+  
+    alias st   status
+    alias stat status
+
+    OPT_TABLE['status'] = 
+    OPT_TABLE['st']     =
+    OPT_TABLE['stat']   = [:changelist, :depth, :ignore_externals, :incremental, :no_ignore,
+                           :quiet, :show_updates, :verbose, :xml]
+  
+
+    # -------------
+    # Other methods
+    # -------------
     
-    
-    def status(path="",options={})
-      changelist       = options[:changelist]       ? "--changelist #{options[:changelist]} " : ""
-      depth            = options[:depth]            ? "--depth #{options[:depth]} " : ""
-      ignore_externals = options[:ignore_externals] ? "--ignore-externals " : ""
-      incremental      = options[:incremental]      ? "--incremental " : ""
-      no_ignore        = options[:no_ignore]        ? "--no-ignore " : ""
-      quiet            = options[:quiet]            ? "--quiet " : ""
-      show_updates     = options[:show_updates]     ? "--show-updates " : ""
-      verbose          = options[:verbose]          ? "--verbose " : ""
-      debug            = options[:debug]
-      cmd = "svn status --xml #{changelist}#{depth}#{ignore_externals}#{incremental}#{no_ignore}#{quiet}#{show_updates}#{verbose} #{path}"
-      @status = XmlSimple.xml_in(`#{cmd}`)["target"][0]
-      if debug
-        puts(@status.to_yaml)
-        puts(@status.inspect)
+    #
+    # history: Print the history of subversion subcommands.
+    #
+    # Calling sequence: history(full = false)
+    #
+    def history(full = false)
+      @history.each_with_index do |h, i|
+        puts("#{i} command: #{h[:command]}")
+        puts("  + output:\n#{h[:output]}") if full
+        puts("  + result: #{h[:result]}")
       end
-#      @status["entry"].each do |e|
-#        e.each_pair {|k,v| puts "#{k.inspect} maps to #{v}"} 
-#      end
-#      `svn status #{@path}`.split("\n").each do |line|
-#        @status[status_filename(line)] = status_type(line)
-#      end
     end
 
 
-    def add
-      status if @status.empty?
-      action("?")
-    end
-    
-    
-    def delete
-      status if @status.empty?
-      action("!")
-    end
-    
-    
-    def info(target="",options={})
-      changelist  = options[:changelist]  ? "--changelist #{options[:changelist]} " : ""
-      depth       = options[:depth]       ? "--depth #{options[:depth]} " : ""
-      incremental = options[:incremental] ? "--incremental " : ""
-      recursive   = options[:recursive]   ? "--recursive " : ""
-      revision    = options[:revision]    ? "--revision #{options[:revision]} " : ""
-      targets     = options[:targets]     ? "--targets #{options[:targets]} " : ""
-      debug       = options[:debug]
-      cmd = "svn info --xml #{changelist}#{depth}#{incremental}#{recursive}#{revision}#{targets} #{target}"
-      @info = XmlSimple.xml_in(`#{cmd}`)["entry"][0]
-      if debug
-        puts(@info.to_yaml)
-        puts(@info.inspect)
-        @info.each_pair {|k,v| puts "#{k.inspect} maps to #{v}"} 
-      end
-    end
-    
-    
-    def list(target="",options={})
-      depth       = options[:depth]       ? "--depth #{options[:depth]} " : ""
-      incremental = options[:incremental] ? "--incremental " : ""
-      recursive   = options[:recursive]   ? "--recursive " : ""
-      revision    = options[:revision]    ? "--revision #{options[:revision]} " : ""
-      verbose     = options[:verbose]     ? "--verbose " : ""
-      debug       = options[:debug]
-      cmd = "svn list --xml #{depth}#{incremental}#{recursive}#{revision}#{verbose} #{target}"
-      @list = XmlSimple.xml_in(`#{cmd}`)["list"][0]
-      if debug
-        puts(@list.to_yaml)
-        puts(@list.inspect)
-      end
-    end
-    
-    
-    def versioned?(path,options={})
-      debug = options[:debug]
+    #
+    # versioned?: Determine if a path is versioned.
+    #
+    # Calling sequence: versioned?(path)
+    #
+    def versioned?(path)
+      return false if @noop
       return false unless File.exist?(path)
-      status(path=path, :depth => "immediates", :debug => debug)
-      return true if @status["entry"].nil?
-      is_versioned = false
-      @status["entry"].each do |e|
-        next unless e["path"] == path
-        wc_status = e["wc-status"][0]
-        puts(wc_status.inspect) if debug
-        is_versioned = (wc_status["item"] != "unversioned")
-        break
-      end
-      is_versioned
+      status(path, :verbose => true, :xml => true)
+      
+      # XML-ise the status info
+      xml = REXML::Document.new((@history.pop)[:output])
+
+      # Target and entry path should be the same
+      # Only one entity at a time is tested
+      target_path = xml.elements["*/target"].attributes["path"]
+      entry_path  = xml.elements["*/target/entry"].attributes["path"]
+      return false unless target_path==entry_path
+      
+      # Check working copy status
+      xml.elements["*/target/entry/wc-status"].attributes["item"] != "unversioned"
     end
     
+  
   private
 
-    # Private methods
-    # ---------------
-    def change(status_type)
-      @status.select {|k,v| status_type.include?(v)}.transpose[0] || []
+  
+    def svn_run_command(command)  #:nodoc:
+      if @noop
+        puts(command)
+        result = 0
+      else
+        output = `#{command}`
+        result = $?
+        @history.push({:command=>command, :output=>output, :result=>result})
+        puts(output)
+      end
+      result == 0
+    end
+  
+  
+    def svn_check_options(options, optdecl)   #:nodoc:
+      h = options.dup
+      optdecl.each do |opt|
+        h.delete opt
+      end
+      raise ArgumentError, "no such option: #{h.keys.join(' ')}" unless h.empty?
+    end
+  
+  
+    def svn_process_options(options)   #:nodoc:
+      option_list = ""
+      options.each do |opt, value|
+        opt_name = opt.to_s
+        case opt
+          # Options with a value
+          when :auto_props, :change, :changelist, :depth, :editor_cmd, :encoding, :file, 
+               :limit, :no_auto_props, :revision, :targets, :with_revprop
+            option_list << "--#{opt_name} #{value} "
+          # Boolean options
+          when :force, :force_log, :keep_changelists, :keep_local, :ignore_externals, :incremental,
+               :no_ignore, :no_unlock, :parents, :quiet, :recursive, :show_updates, :stop_on_copy,
+               :use_merge_history, :verbose, :with_all_revprops, :with_no_revprops, :xml
+            option_list << "--#{opt_name} " if value
+          # Special treatment
+          when :message
+            option_list << "--#{opt_name} \"#{value}\" "
+          else
+            raise ArgumentError, "argument #{opt_name} invalid or not defined yet!!"
+        end
+      end
+      option_list.rstrip
     end
 
-    
-    def action(status_type)
-      action_type = ACTION[status_type]
-      change(status_type).each do |f|
-        system("svn #{action_type} #{f}")
-      end if action_type
-    end
-
-
-    def status_type(line)
-      line[0].chr
-    end
-    
-    
-    def status_filename(line)
-      line[7..-1].chomp
-    end
-
-
-    def parse_options(options={})
-      return if options.empty?
-    end
-    
-  end
-
-end
+  end  # Class Svn
+  
+end  # Module Svu_Util
