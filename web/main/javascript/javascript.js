eff654dfcab5fc3054ab2863cca64b7dd354dc1b
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2012-01-03 18:07:50 +0000

    * web: Deleting main-level web directory. Never used.
    Former-commit-id: d251ca18f92ba2a29306b9ec2e38f7ede9c5979d

--- web/main/javascript/javascript.js
+++
@@ -1,7 +0,0 @@
-EventUtil.addEventHandler(window, 'load', onWindowLoad);
-
-//------------------------------------------
-function onWindowLoad() {
-  var category = document.getElementById('category').getAttribute('value');
-  var current_category = document.getElementById(category).setAttribute('class', 'current_category');
-}
