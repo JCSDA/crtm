597dfb899ec39b1aabdc0389b42716f6782bf8f3
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2013-11-25 14:11:19 +0000

    src/InstrumentInfo/Instruments/CrIS
      * CrIS_Define.f90: EOL whitespace removed by editor. Added USE statement
        for apodisation utility module. Renamed CrIS-specific apodisation type
        parameter identifiers to the generic name from the apodisation utility,
        removing the Blackman-Harris-4 parameter as well.
        (CrIS_ApodFunction): Modified to use the apodisation function imported
        from the apodisation utility module.
    
    
    
    Former-commit-id: afc451ae81caf02ce55e9e306e01cc870d3646e5

--- src/InstrumentInfo/Instruments/CrIS/CrIS_Define.f90
+++ src/InstrumentInfo/Instruments/CrIS/CrIS_Define.f90
@@ -18,18 +18,21 @@ MODULE CrIS_Define
   USE Type_Kinds           , ONLY: fp
   USE Message_Handler      , ONLY: SUCCESS, FAILURE, Display_Message
   USE Fundamental_Constants, ONLY: PI, LN2
+  USE Apodisation_Utility  , ONLY: HAMMING, &
+                                   BLACKMANHARRIS_3, &
+                                   Apodisation_Function
+  USE SPC_IFG_Utility      , ONLY: ComputeMeanDelta
   ! Disable implicit typing
   IMPLICIT NONE
-  
+
   ! ----------
   ! Visibility
   ! ----------
   ! Everything is default private
   PRIVATE
   ! Public parameters
-  PUBLIC :: CRIS_HAMMING
-  PUBLIC :: CRIS_BLACKMANHARRIS_3
-  PUBLIC :: CRIS_BLACKMANHARRIS_4
+  PUBLIC :: HAMMING
+  PUBLIC :: BLACKMANHARRIS_3
   PUBLIC :: N_CRIS_BANDS
   PUBLIC :: N_CRIS_CHANNELS
   ! Public module procedures
@@ -48,13 +51,13 @@ MODULE CrIS_Define
   PUBLIC :: CrIS_Remove_Guard_Channels
   PUBLIC :: CrIS_BandName
   PUBLIC :: CrIS_DefineVersion
-  
-  INTERFACE CrIS_Remove_Guard_Channels  
+
+  INTERFACE CrIS_Remove_Guard_Channels
     MODULE PROCEDURE integer_rgc
     MODULE PROCEDURE real_rgc
   END INTERFACE CrIS_Remove_Guard_Channels
-  
-  
+
+
   ! -----------------
   ! Module parameters
   ! -----------------
@@ -71,11 +74,6 @@ MODULE CrIS_Define
   REAL(fp), PARAMETER :: THREE     = 3.0_fp
   REAL(fp), PARAMETER :: HUNDRED   = 100.0_fp
   REAL(fp), PARAMETER :: M2CM      = HUNDRED
-
-  ! Apodisation function types
-  INTEGER, PARAMETER :: CRIS_HAMMING          = 1
-  INTEGER, PARAMETER :: CRIS_BLACKMANHARRIS_3 = 2
-  INTEGER, PARAMETER :: CRIS_BLACKMANHARRIS_4 = 3
 
   ! Instrument parameters
   ! ...Number of bands and channels
@@ -97,7 +95,7 @@ MODULE CrIS_Define
   REAL(fp), PARAMETER :: NOMINAL_MAXX_IN_M(N_CRIS_BANDS) = &
                          (/ 8.03520e-03_fp, &
                             4.09200e-03_fp, &
-                            2.01500e-03_fp /) 
+                            2.01500e-03_fp /)
   REAL(fp), PARAMETER :: NOMINAL_MAXX(N_CRIS_BANDS) = NOMINAL_MAXX_IN_M * M2CM
 
   ! Band parameters
@@ -123,14 +121,14 @@ MODULE CrIS_Define
   INTEGER, PARAMETER :: N_GCHANNELS_PER_BAND(N_CRIS_BANDS) = (/ 864,  528,  200 /)
   INTEGER, PARAMETER :: MAX_N_BAND_GCHANNELS = 864
 
-  
+
   ! Parameters for the resampled frequency grid
   REAL(fp), PARAMETER :: MIN_FREQUENCY = 650.0_fp
   REAL(fp), PARAMETER :: MAX_FREQUENCY = 2550.0_fp
   REAL(fp), PARAMETER :: D_FREQUENCY(N_CRIS_BANDS) = (/ 0.625_fp, 1.25_fp, 2.5_fp /)
   REAL(fp), PARAMETER :: RESAMPLED_MAXX(N_CRIS_BANDS)   = (/ 0.8_fp  , 0.4_fp , 0.2_fp /)
-  
-  
+
+
 CONTAINS
 
 
@@ -175,10 +173,10 @@ CONTAINS
 
     ! Setup
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
-    
+
     ! Select the number of points
     n = N_FFT(ib)
-    
+
   END FUNCTION CrIS_nFFT
 
 
@@ -244,7 +242,7 @@ CONTAINS
     ELSE
       maxX = NOMINAL_MAXX(ib)
     END IF
-    
+
   END FUNCTION CrIS_MaxX
 
 
@@ -297,11 +295,11 @@ CONTAINS
 !
 !          X=dx-maxX     X=0cm           X=maxX
 !              |         (ZPD)             |
-!              |           |               | 
+!              |           |               |
 !              v           v               v
 !
 !              x   x   x   #   o   o   o   o
-!                                   
+!
 !                      --->|   |<---
 !                           dx
 !:sdoc-:
@@ -317,7 +315,7 @@ CONTAINS
     REAL(fp) :: maxX
     INTEGER :: i, nHalf
     INTEGER :: ib
-    
+
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
     ! Get the number of positive delays
     nHalf = n/2
@@ -337,7 +335,7 @@ CONTAINS
 !       CrIS_ApodFunction
 !
 ! PURPOSE:
-!       Pure function to compute the CrIS apodisation function for a given 
+!       Pure function to compute the CrIS apodisation function for a given
 !       optical delay grid.
 !
 ! CALLING SEQUENCE:
@@ -359,18 +357,18 @@ CONTAINS
 !                  ATTRIBUTES: INTENT(IN)
 !
 ! OPTIONAL INPUTS:
-!       apodType:  Set this argument to the defined parameter values to    
-!                  select the type of apodisation function.                
+!       apodType:  Set this argument to the defined parameter values to
+!                  select the type of apodisation function.
 !                     == HAMMING          for Hamming apodisation [DEFAULT]
 !                     == BLACKMANHARRIS_3 for Blackman-Harris 3-term apodisation
 !                     == BLACKMANHARRIS_4 for Blackman-Harris 4-term apodisation
 !                  If not specified, or any other value is supplied, then
 !                  the computed apodisation uses HAMMING.
-!                  UNITS:      N/A                                         
-!                  TYPE:       INTEGER                                     
-!                  DIMENSION:  Scalar                                      
-!                  ATTRIBUTES: INTENT(IN), OPTIONAL                        
-!                    
+!                  UNITS:      N/A
+!                  TYPE:       INTEGER
+!                  DIMENSION:  Scalar
+!                  ATTRIBUTES: INTENT(IN), OPTIONAL
+!
 !       nominal:   Set this argument to use the nominal value of the CRIS
 !                  max. OPD rather than the resampled max. OPD..
 !                  If == .FALSE., the resampled value is used. [DEFAULT]
@@ -388,14 +386,14 @@ CONTAINS
 !                  DIMENSION:  Same as input x argument.
 !
 ! COMMENTS:
-!       The Hamming apodization function is a reasonable and efficient 
+!       The Hamming apodization function is a reasonable and efficient
 !       function to use in atmospheric remote sensing applications with
 !       high signal-to-noise instruments, both because its channel response
-!       funciton has side-lobes less than 1% of the central lobe and because
-!       it has a well behaved analytic inverse transformation which 
+!       function has side-lobes less than 1% of the central lobe and because
+!       it has a well behaved analytic inverse transformation which
 !       satisfies retrieval models and also allows apodized radiances to
 !       be returned to their unapodized values.
-!       
+!
 !       The Blackman-Harris apodizations produce high lobe suppression functions
 !       and can be considered among the top performers in commonly used FTIR
 !       apodisation filters.
@@ -410,62 +408,24 @@ CONTAINS
     ! Function result
     REAL(fp) :: afn(SIZE(x))
     ! Local variables
-    INTEGER :: atype
-    REAL(fp) :: a0, a1, a2, a3
-    REAL(fp) :: maxX
+    INTEGER  :: atype
+    REAL(fp) :: xmax
     REAL(fp) :: xnorm(SIZE(x))
-    INTEGER :: ib
-    
+    INTEGER  :: ib
+
     ! Setup
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
-    maxX = CrIS_MaxX(ib, nominal=nominal)
-    xnorm = x/maxX
+    xmax = CrIS_MaxX(ib, nominal=nominal)
+    xnorm = x/xmax
     ! ...Set apodisation type
-    atype = -1 ! Force default
+    atype = HAMMING
     IF ( PRESENT(apodType) ) atype = apodType
 
-
     ! Compute apodisation function
-    SELECT CASE(atype)
-    
-      CASE(CRIS_BLACKMANHARRIS_3)
-        a0 = 0.42323_fp
-        a1 = 0.49755_fp
-        a2 = 0.07922_fp
-        WHERE ( ABS(x) <= maxX )
-          afn = a0 + a1*COS(PI*xnorm) + &
-                     a2*COS(TWO*PI*xnorm)
-        ELSEWHERE
-          afn = ZERO
-        END WHERE
-       
-      CASE(CRIS_BLACKMANHARRIS_4)
-        a0 = 0.35875_fp
-        a1 = 0.48829_fp
-        a2 = 0.14128_fp
-        a3 = 0.01168_fp
-        WHERE ( ABS(x) <= maxX )
-          afn = a0 + a1*COS(PI*xnorm)     + &
-                     a2*COS(TWO*PI*xnorm) + &
-                     a3*COS(THREE*PI*xnorm)
-        ELSEWHERE
-          afn = ZERO
-        END WHERE
-     
-      ! ...Hamming apodisation is the default 
-      CASE DEFAULT
-        a0 = 0.54_fp
-        a1 = 0.46_fp
-        WHERE ( ABS(x) <= maxX )
-          afn = a0 + a1*COS(PI*xnorm)
-        ELSEWHERE
-          afn = ZERO
-        END WHERE
-      
-    END SELECT
-    
+    afn = Apodisation_Function(x,aType=aType,xMax=xmax)
+
   END FUNCTION CrIS_ApodFunction
-  
+
 
 !--------------------------------------------------------------------------------
 !:sdoc+:
@@ -518,15 +478,15 @@ CONTAINS
 
     ! Setup
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
-    
+
     ! Select frequencies and interval
     f1 = CrIS_BeginF(ib, include_guard_channels=include_guard_channels)
     f2 = CrIS_EndF(  ib, include_guard_channels=include_guard_channels)
     df = CrIS_dF(ib)
-    
+
     ! Compute the points
     n = INT((f2-f1)/df + ONEPOINT5)
-    
+
   END FUNCTION CrIS_nPts
 
 
@@ -578,10 +538,10 @@ CONTAINS
     ! Local variables
     INTEGER :: i, ib
     REAL(fp) :: f1, df
-    
+
     ! Setup
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
-    
+
     ! Select begin frequency and interval
     f1 = CrIS_BeginF(ib, include_guard_channels=include_guard_channels)
     df = CrIS_dF(ib)
@@ -590,10 +550,10 @@ CONTAINS
     DO i = 1, CrIS_nPts(ib,include_guard_channels)
       f(i) = f1 + (df*REAL(i-1,fp))
     END DO
-    
+
   END FUNCTION CrIS_F
-  
-  
+
+
 !--------------------------------------------------------------------------------
 !:sdoc+:
 !
@@ -643,14 +603,14 @@ CONTAINS
 
     ! Setup
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
-    
+
     ! Select channel bounds
     ic1 = CrIS_BeginChannel(ib, include_guard_channels=include_guard_channels)
     ic2 = CrIS_EndChannel(ib, include_guard_channels=include_guard_channels)
-    
+
     ! Construct channel array
     ch = (/(i, i=ic1,ic2)/)
-    
+
   END FUNCTION CrIS_Channels
 
 
@@ -704,7 +664,7 @@ CONTAINS
     CHARACTER(ML) :: msg
     INTEGER :: ib, n
     INTEGER :: i1, i2
-    
+
     ! Setup
     output_vector = -1
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
@@ -714,14 +674,14 @@ CONTAINS
       WRITE( msg,'("Input vector size (",i0,") inconsistent for CrIS band ",i0)') n, ib
       CALL Display_Message( ROUTINE_NAME, msg, FAILURE ); RETURN
     END IF
-    
+
     ! Pick out the data
     i1 = N_GUARD_CHANNELS(1, ib) + 1
     i2 = n - N_GUARD_CHANNELS(2, ib)
     output_vector = input_vector(i1:i2)
-    
+
   END FUNCTION integer_rgc
-  
+
   FUNCTION real_rgc(band, input_vector) RESULT(output_vector)
     ! Arguments
     INTEGER , INTENT(IN) :: band
@@ -734,7 +694,7 @@ CONTAINS
     CHARACTER(ML) :: msg
     INTEGER :: ib, n
     INTEGER :: i1, i2
-    
+
     ! Setup
     output_vector = -1.0_fp
     ib = MAX(MIN(band,N_CRIS_BANDS),1)
@@ -744,12 +704,12 @@ CONTAINS
       WRITE( msg,'("Input vector size (",i0,") inconsistent for CrIS band ",i0)') n, ib
       CALL Display_Message( ROUTINE_NAME, msg, FAILURE ); RETURN
     END IF
-    
+
     ! Pick out the data
     i1 = N_GUARD_CHANNELS(1, ib) + 1
     i2 = n - N_GUARD_CHANNELS(2, ib)
     output_vector = input_vector(i1:i2)
-    
+
   END FUNCTION real_rgc
 
 
@@ -815,7 +775,7 @@ CONTAINS
     ELSE
       f1 = BAND_GF1(ib)
     END IF
-    
+
   END FUNCTION CrIS_BeginF
 
 
@@ -881,7 +841,7 @@ CONTAINS
     ELSE
       f2 = BAND_GF2(ib)
     END IF
-    
+
   END FUNCTION CrIS_EndF
 
 
@@ -927,7 +887,7 @@ CONTAINS
 
     ! Retrieve the frequency interval
     df = D_FREQUENCY(ib)
-    
+
   END FUNCTION CrIS_dF
 
 
@@ -994,7 +954,7 @@ CONTAINS
     ELSE
       ch1 = BEGIN_GCHANNEL(ib)
     END IF
-    
+
   END FUNCTION CrIS_BeginChannel
 
 
@@ -1060,7 +1020,7 @@ CONTAINS
     ELSE
       ch2 = END_GCHANNEL(ib)
     END IF
-    
+
   END FUNCTION CrIS_EndChannel
 
 
@@ -1105,7 +1065,7 @@ CONTAINS
 
     ! Retrieve the band name
     name = BAND_NAME(ib)
-    
+
   END FUNCTION CrIS_BandName
 
 
