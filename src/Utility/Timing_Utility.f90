d674c6a7cd5b08e5d3024f80270862ca504fc9f8
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2014-02-05 16:39:43 +0000

    src/Utility
      * Timing_Utility: Added USE of DateTime_Utility module. Added "xTiming_WriteFile"
        procedure to the public list and uncommented out the procedure in question and
        temporarily renamed it to "xTiming_WriteFile".
        (Timing_Display): Modified procedure to use the new Timing_ToString function
        rather than constructing the output string itself.
        (Timing_ToString): New procedure to return the equivalent string representation
        of a Timing object.
        (xTiming_WriteFile): Completed procedure to output timing object arrays to file.
        (Timing_ElapsedTime): Private elemental function to computed the elapsed time in seconds.
    
    
    
    Former-commit-id: 499eebc8d4c64d104d232ed77e11136083ef9cd3

--- src/Utility/Timing_Utility.f90
+++ src/Utility/Timing_Utility.f90
@@ -4,10 +4,12 @@
 MODULE Timing_Utility
 
   ! Module usage
-  USE Type_Kinds     , ONLY: fp
-  USE Message_Handler, ONLY: SUCCESS, INFORMATION, FAILURE, Display_Message
-  USE File_Utility   , ONLY: Get_Lun
-
+  USE Type_Kinds      , ONLY: fp
+  USE Message_Handler , ONLY: SUCCESS, INFORMATION, FAILURE, Display_Message
+  USE File_Utility    , ONLY: Get_Lun, File_Exists
+  USE DateTime_Utility, ONLY: DateTime_type    , &
+                              DateTime_Now     , &
+                              DateTime_ToString
   ! Disable all implicit typing
   IMPLICIT NONE
 
@@ -23,7 +25,7 @@ MODULE Timing_Utility
   PUBLIC :: Timing_Inspect
   PUBLIC :: Timing_Set
   PUBLIC :: Timing_Get
-  PUBLIC :: Timing_WriteFile
+  PUBLIC :: Timing_WriteFile, xTiming_WriteFile
   ! ...Old named procedures
   PUBLIC :: Begin_Timing
   PUBLIC :: End_Timing
@@ -163,50 +165,28 @@ CONTAINS
 !:sdoc-:
 !--------------------------------------------------------------------------------
 
-  SUBROUTINE Timing_Display( self  , &  ! Input
-                             Caller  )  ! Optional input
+  SUBROUTINE Timing_Display( &
+    self  , &  ! Input
+    Caller  )  ! Optional input
     ! Arguments
     TYPE(Timing_type),      INTENT(IN) :: self
     CHARACTER(*), OPTIONAL, INTENT(IN) :: Caller
-    ! Local parameters
-    REAL(fp), PARAMETER :: N_SECONDS_IN_HOUR        = 3600.0_fp
-    REAL(fp), PARAMETER :: N_SECONDS_IN_MINUTE      =   60.0_fp
-    REAL(fp), PARAMETER :: N_MILLISECONDS_IN_SECOND = 1000.0_fp
     ! Local variables
-    CHARACTER(ML) :: Routine_Name
-    CHARACTER(ML) :: Elapsed_Time
-    REAL(fp)      :: Total_Time
-    INTEGER       :: n_Hours
-    INTEGER       :: n_Minutes
-    INTEGER       :: n_Seconds
-    INTEGER       :: n_milliSeconds
+    CHARACTER(ML) :: msg
+    CHARACTER(ML) :: routine_name
 
     ! Set up
-    Routine_Name = 'Timing_Display'
-    IF ( PRESENT(Caller) ) Routine_Name = TRIM(ADJUSTL(Caller))
+    routine_name = 'Timing_Display'
+    IF ( PRESENT(Caller) ) routine_name = TRIM(ADJUSTL(Caller))
     ! ...Check if timing structure valid for display
     IF ( .NOT. self%Is_Valid ) THEN
-      CALL Display_Message( TRIM(Routine_Name), &
-                            '***** Invalid timing structure! *****', &
-                            FAILURE )
-      RETURN
+      msg = 'Invalid timing structure!'
+      CALL Display_Message( routine_name, msg, FAILURE ); RETURN
     END IF
 
-    ! Compute the total time in seconds
-    Total_Time = REAL(self%End_Clock - self%Begin_Clock, fp) / REAL(self%Hertz, fp)
-
-    ! Split the total time into hours, minutes, seconds, and millseconds
-    n_Hours        = INT(Total_Time / N_SECONDS_IN_HOUR)
-    n_Minutes      = INT(MOD(Total_Time,N_SECONDS_IN_HOUR) / N_SECONDS_IN_MINUTE)
-    n_Seconds      = INT(MOD(MOD(Total_Time,N_SECONDS_IN_HOUR), N_SECONDS_IN_MINUTE))
-    n_milliSeconds = INT((Total_Time - AINT(Total_Time,fp)) * N_MILLISECONDS_IN_SECOND)
-
     ! Construct the character string
-    WRITE( Elapsed_Time, '("Elapsed time-- ",i2.2,":",i2.2,":",i2.2,".",i3.3 )' ) &
-                         n_Hours, n_Minutes, n_Seconds, n_milliSeconds
-    CALL Display_Message( TRIM(Routine_Name), &
-                          TRIM(Elapsed_Time), &
-                          INFORMATION )
+    WRITE( msg, '("Elapsed time-- ",a)' ) TRIM(Timing_ToString(self))
+    CALL Display_Message( routine_name, msg, INFORMATION )
 
   END SUBROUTINE Timing_Display
 
@@ -243,6 +223,108 @@ CONTAINS
   END SUBROUTINE Timing_Inspect
 
 
+!------------------------------------------------------------------------------
+!:sdoc+:
+!
+! NAME:
+!       Timing_ToString
+!
+! PURPOSE:
+!       Elemental function to return the equivalent string representation
+!       of the Timing object.
+!
+! CALLING SEQUENCE:
+!       string = Timing_ToString( Timing, Seconds=seconds, Fmt_String=fmt_string )
+!
+! OBJECTS:
+!   Timing:        Timing structure containing the timed data.
+!                  UNITS:      N/A
+!                  TYPE:       Timing_type
+!                  DIMENSION:  Scalar or any rank
+!                  ATTRIBUTES: INTENT(IN)
+!
+! OPTIONAL INPUTS:
+!   Seconds:       Set this logical argument to create a string that is
+!                  simply the number of seconds of elpased time.
+!                  If == .FALSE., the output string format is HH:MM:SS.sss [DEFAULT].
+!                     == .TRUE.,  the output string format is XX.XXXXXXe+EE (es13.6).
+!                  If not specified, default is .FALSE.
+!                  UNITS:      N/A
+!                  TYPE:       LOGICAL
+!                  DIMENSION:  Conformable with the Timing input
+!                  ATTRIBUTES: INTENT(IN), OPTIONAL
+!
+!   Fmt_String:    Character string holding the format string to use for
+!                  seconds of elasped time output. If not supplied, the
+!                  default is '(es13.6)'.
+!                  This argument is ignored if the "Seconds" argument is
+!                  not specified, or not set.
+!                  UNITS:      N/A
+!                  TYPE:       CHARACTER(*)
+!                  DIMENSION:  Conformable with the Timing input.
+!                  ATTRIBUTES: INTENT(IN), OPTIONAL
+!
+! FUNCTION RESULT:
+!   string:        String containing the time elapsed in seconds.
+!                  UNITS:      N/A
+!                  TYPE:       CHARACTER(80)
+!                  DIMENSION:  Conformable with input Timing argument.
+!
+!:sdoc-:
+!------------------------------------------------------------------------------
+
+  ELEMENTAL FUNCTION Timing_ToString( &
+    self      , &  ! Input
+    Seconds   , &  ! Optional input
+    Fmt_String) &  ! Optional input
+  RESULT( string )
+    ! Arguments
+    TYPE(Timing_type),           INTENT(IN) :: self
+    LOGICAL          , OPTIONAL, INTENT(IN) :: Seconds
+    CHARACTER(*)     , OPTIONAL, INTENT(IN) :: Fmt_String
+    ! Function result
+    CHARACTER(80) :: string
+    ! Local parameters
+    REAL(fp), PARAMETER :: N_SECONDS_IN_HOUR        = 3600.0_fp
+    REAL(fp), PARAMETER :: N_SECONDS_IN_MINUTE      =   60.0_fp
+    REAL(fp), PARAMETER :: N_MILLISECONDS_IN_SECOND = 1000.0_fp
+    ! Local variables
+    CHARACTER(80) :: fmt
+    LOGICAL  :: hhmmss
+    REAL(fp) :: total_time
+    INTEGER  :: n_hours
+    INTEGER  :: n_minutes
+    INTEGER  :: n_seconds
+    INTEGER  :: n_milliseconds
+
+    ! Setup
+    ! ...Process keyword arguments
+    hhmmss = .TRUE.
+    IF ( PRESENT(Seconds) ) hhmmss = .NOT. Seconds
+    fmt = '(es13.6)'
+    IF ( PRESENT(Fmt_String) ) fmt = ADJUSTL(Fmt_String)
+
+
+    ! Compute the total time in seconds
+    total_time = Timing_ElapsedTime( self )
+
+
+    ! Create the string
+    IF ( hhmmss ) THEN
+      ! ...Split the total time into hours, minutes, seconds, and millseconds
+      n_hours        = INT(total_time / N_SECONDS_IN_HOUR)
+      n_minutes      = INT(MOD(total_time,N_SECONDS_IN_HOUR) / N_SECONDS_IN_MINUTE)
+      n_seconds      = INT(MOD(MOD(total_time,N_SECONDS_IN_HOUR), N_SECONDS_IN_MINUTE))
+      n_milliseconds = INT((total_time - AINT(total_time,fp)) * N_MILLISECONDS_IN_SECOND)
+      ! ...Construct the HH:MM:SS.sss string
+      WRITE( string,'(i2.2,":",i2.2,":",i2.2,".",i3.3 )' ) &
+                    n_hours, n_minutes, n_seconds, n_milliseconds
+    ELSE
+      ! ...Construct the number of seconds string
+      WRITE( string,FMT=fmt ) total_time
+    END IF
+
+  END FUNCTION Timing_ToString
 
 
   ! Subroutine to set the components of a timing object
@@ -287,7 +369,7 @@ CONTAINS
     IF ( PRESENT(End_Clock  ) ) End_Clock   = self%End_Clock
     IF ( PRESENT(Is_Valid   ) ) Is_Valid    = self%Is_Valid
   END SUBROUTINE Timing_Get
-  
+
   ! Subroutine to write timing information in
   ! seconds and the date for a given timing object
   SUBROUTINE Timing_WriteFile( &
@@ -309,7 +391,7 @@ CONTAINS
     OPEN(Unit=LUN, FILE=filename, POSITION="APPEND", Action="WRITE")
     WRITE(LUN, FMT='(1X, A10, 3X, A10, F12.2)') date, time, Total_Time
     CLOSE(LUN)
-  END SUBROUTINE Timing_WriteFile  
+  END SUBROUTINE Timing_WriteFile
 
 !--------------------------------------------------------------------------------
 !:sdoc+:
@@ -376,107 +458,160 @@ CONTAINS
 !:sdoc-:
 !--------------------------------------------------------------------------------
 
-!  FUNCTION Timing_WriteFile( &
-!    Timing_Array, &
-!    Filename    , &
-!    Clobber     , &
-!    Heading     ) &
-!  RESULT( err_stat )
-!    ! Arguments
-!    TYPE(Timing_type),           INTENT(IN) :: Timing_Array(:)
-!    CHARACTER(*)     ,           INTENT(IN) :: Filename
-!    LOGICAL          , OPTIONAL, INTENT(IN) :: Clobber
-!    CHARACTER(*)     , OPTIONAL, INTENT(IN) :: Heading(:)
-!    ! Function result
-!    INTEGER :: err_stat
-!    ! Local parameters
-!    CHARACTER(*), PARAMETER :: ROUTINE_NAME = 'Timing_WriteFile'
-!    ! Local variables
-!    CHARACTER(ML) :: msg, err_msg, io_msg
-!    CHARACTER(ML), ALLOCATABLE :: title(:)
-!    CHARACTER(8) :: status, position
-!    CHARACTER(8)  :: date
-!    CHARACTER(10) :: time
-!    LOGICAL :: append
-!    INTEGER :: fid
-!    INTEGER :: alloc_stat, io_stat
-!    INTEGER :: i, n_times
-!
-!    ! Set up
-!    err_stat = SUCCESS
-!    ! ...Check structures
-!    IF ( .NOT. ALL(Timing_Array%Is_Valid) ) THEN
-!      err_stat = FAILURE
-!      msg = 'Input timing array contains invalid elements'
-!      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
-!    END IF
-!    n_times = SIZE(Timing_Array)
-!    ! ...Check clobber argument
-!    append = .TRUE.
-!    IF ( PRESENT(Clobber) ) append = .NOT. Clobber
-!    ! ...Check header argument
-!    ALLOCATE(title(n_times), STAT=alloc_stat, ERRMSG=err_msg)
-!    IF ( alloc_stat /= 0 ) THEN
-!      err_stat = FAILURE
-!      msg = 'Local title array allocation failed - '//TRIM(err_msg)
-!      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
-!    END IF
-!    IF ( PRESENT(Heading) ) THEN
-!      IF ( SIZE(Heading) /= n_times ) THEN
-!        err_stat = FAILURE
-!        msg = 'Input heading array different size from timing array'
-!        CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
-!      END IF
-!      title = Heading
-!    ELSE
-!      DO i = 1, n_times
-!        WRITE(title(i),'("Time ",i0)') i
-!      END DO
-!    END IF
-!
-!
-!    ! Open the file
-!    IF ( append ) THEN
-!      status   = 'UNKNOWN'
-!      position = 'APPEND'
-!    ELSE
-!      status   = 'REPLACE'
-!      position = 'REWIND'
-!    END IF
-!    fid = Get_Lun()
-!    IF ( fid < 0 ) THEN
-!      err_stat = FAILURE
-!      msg = 'Error obtaining free logical unit number'
-!      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
-!    END IF
-!    OPEN( fid, FILE     = filename   , &
-!               FORM     = 'FORMATTED', &
-!               STATUS   = status     , &
-!               POSITION = position   , &
-!               IOSTAT   = io_stat    , &
-!               IOMSG    = io_msg       )
-!    IF ( io_stat /= 0 ) THEN
-!      err_stat = FAILURE
-!      msg = 'Error opening file '//TRIM(filename)//' - '//TRIM(io_msg)
-!      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
-!    END IF
-!
-!
-!    ! Get the current date/time for output
-!    CALL DATE_AND_TIME(DATE=date, TIME=time)
-!print *, date, ' ', time
-!
-!
-!    ! Done
-!    CLOSE(fid, IOSTAT = io_stat, &
-!               IOMSG  = io_msg   )
-!    IF ( io_stat /= 0 ) THEN
-!      err_stat = FAILURE
-!      msg = 'Error closing file '//TRIM(filename)//' - '//TRIM(io_msg)
-!      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
-!    END IF
-!
-!  END FUNCTION Timing_WriteFile
+  FUNCTION xTiming_WriteFile( &
+    Timing_Array, &
+    Filename    , &
+    Clobber     , &
+    Heading     ) &
+  RESULT( err_stat )
+    ! Arguments
+    TYPE(Timing_type),           INTENT(IN) :: Timing_Array(:)
+    CHARACTER(*)     ,           INTENT(IN) :: Filename
+    LOGICAL          , OPTIONAL, INTENT(IN) :: Clobber
+    CHARACTER(*)     , OPTIONAL, INTENT(IN) :: Heading(:)
+    ! Function result
+    INTEGER :: err_stat
+    ! Local parameters
+    CHARACTER(*), PARAMETER :: ROUTINE_NAME = 'Timing_WriteFile'
+    ! Local variables
+    CHARACTER(ML) :: msg, io_msg
+    CHARACTER(ML) :: current_time
+    CHARACTER(ML) :: title(SIZE(Timing_Array))
+    CHARACTER(ML) :: elapsed_time(SIZE(Timing_Array))
+    CHARACTER(ML) :: output_fmt
+    CHARACTER(8) :: status, position
+    LOGICAL :: append
+    INTEGER :: fid
+    INTEGER :: io_stat
+    INTEGER :: i, n_times
+    INTEGER :: sl_current_time
+    INTEGER :: sl_elapsed_time
 
+    ! Set up
+    err_stat = SUCCESS
+    ! ...Check structures
+    IF ( .NOT. ALL(Timing_Array%Is_Valid) ) THEN
+      err_stat = FAILURE
+      msg = 'Input timing array contains invalid elements'
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
+    END IF
+    n_times = SIZE(Timing_Array)
+    ! ...Check clobber argument
+    append = .TRUE.
+    IF ( PRESENT(Clobber) ) append = .NOT. Clobber
+    ! ...Check header argument
+    IF ( PRESENT(Heading) ) THEN
+      IF ( SIZE(Heading) /= n_times ) THEN
+        err_stat = FAILURE
+        msg = 'Input heading array different size from timing array'
+        CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
+      END IF
+      title = Heading
+    ELSE
+      DO i = 1, n_times
+        WRITE(title(i),'("Time ",i0)') i
+      END DO
+    END IF
+
+
+    ! Open the file
+    ! ...Set specifiers for write
+    IF ( append ) THEN
+      status   = 'OLD'
+      position = 'APPEND'
+    ELSE
+      status   = 'REPLACE'
+      position = 'REWIND'
+    END IF
+    ! ...Special case for a file that doesn't exist yet
+    IF ( .NOT. File_Exists(filename) ) status = 'NEW'
+    ! ...Get a free unit number
+    fid = Get_Lun()
+    IF ( fid < 0 ) THEN
+      err_stat = FAILURE
+      msg = 'Error obtaining free logical unit number'
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
+    END IF
+    ! ...Open the file for output
+    OPEN( fid, FILE     = filename   , &
+               FORM     = 'FORMATTED', &
+               STATUS   = status     , &
+               POSITION = position   , &
+               IOSTAT   = io_stat    , &
+               IOMSG    = io_msg       )
+    IF ( io_stat /= 0 ) THEN
+      err_stat = FAILURE
+      msg = 'Error opening file '//TRIM(filename)//' - '//TRIM(io_msg)
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
+    END IF
+
+
+    ! Get the current date/time string, and its length, for output
+    current_time    = DateTime_ToString(DateTime_Now(),Format='s')
+    sl_current_time = LEN_TRIM(current_time)
+
+
+    ! Transform the timing info to strings, and get their length
+    elapsed_time    = Timing_ToString(Timing_Array,Seconds=.TRUE.)
+    sl_elapsed_time = MAX(MAXVAL(LEN_TRIM(elapsed_time)), MAXVAL(LEN_TRIM(title)))
+
+
+    ! Construct the output write format
+    WRITE(output_fmt,'("(a",i0,",",i0,"(2x,a",i0,"))")') &
+                     sl_current_time, n_times, sl_elapsed_time
+
+
+    ! Write the header line if necessary
+    IF ( TRIM(status) == 'NEW' ) THEN
+      WRITE(fid,FMT = output_fmt, IOSTAT = io_stat, IOMSG = io_msg) &
+        'Current time', (TRIM(title(i)), i = 1, n_times)
+      IF ( io_stat /= 0 ) THEN
+        err_stat = FAILURE
+        msg = 'Error writing header to '//TRIM(filename)//' - '//TRIM(io_msg)
+        CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
+      END IF
+    END IF
+
+
+    ! Write the current set of times to file
+    WRITE(fid,FMT = output_fmt, IOSTAT = io_stat, IOMSG = io_msg) &
+      TRIM(current_time), &
+      (TRIM(elapsed_time(i)), i = 1, n_times)
+    IF ( io_stat /= 0 ) THEN
+      err_stat = FAILURE
+      msg = 'Error writing timing data to '//TRIM(filename)//' - '//TRIM(io_msg)
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
+    END IF
+
+
+    ! Done
+    CLOSE(fid, IOSTAT = io_stat, &
+               IOMSG  = io_msg   )
+    IF ( io_stat /= 0 ) THEN
+      err_stat = FAILURE
+      msg = 'Error closing file '//TRIM(filename)//' - '//TRIM(io_msg)
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat); RETURN
+    END IF
+
+  END FUNCTION xTiming_WriteFile
+
+
+
+!##############################################################################
+!##############################################################################
+!##                                                                          ##
+!##                      ## PRIVATE MODULE ROUTINES ##                       ##
+!##                                                                          ##
+!##############################################################################
+!##############################################################################
+
+  ! Private elemental function to compute the elapsed time in seconds
+  ELEMENTAL FUNCTION Timing_ElapsedTime( self ) RESULT( Elapsed_Time )
+    TYPE(Timing_type), INTENT(IN) :: self
+    REAL(fp) :: Elapsed_Time
+    Elapsed_Time = 0.0_fp
+    IF ( .NOT. self%Is_Valid ) RETURN
+    Elapsed_Time = REAL(self%End_Clock - self%Begin_Clock, fp) / REAL(self%Hertz, fp)
+  END FUNCTION Timing_ElapsedTime
 
 END MODULE Timing_Utility
