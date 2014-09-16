f2b8cffae803de10e009661390a45c9213752afb
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2014-09-16 18:24:33 +0000

    src/InstrumentInfo/oSRF/PtrArr_Utility
      * PtrArr_Define.f90: Updated PUBLIC list. Added brief header comments to
        the various procedures.
        (PtrArr_FromVector): New.
        (PtrArr_ToVector): New.
    
    
    
    Former-commit-id: 35e20c513e1da34039614b55c702f62c786e6421

--- src/InstrumentInfo/oSRF/PtrArr_Utility/PtrArr_Define.f90
+++ src/InstrumentInfo/oSRF/PtrArr_Utility/PtrArr_Define.f90
@@ -44,6 +44,8 @@ MODULE PtrArr_Define
   PUBLIC :: PtrArr_Create
   PUBLIC :: PtrArr_SetValue
   PUBLIC :: PtrArr_GetValue
+  PUBLIC :: PtrArr_FromVector
+  PUBLIC :: PtrArr_ToVector
   PUBLIC :: PtrArr_Inspect
   PUBLIC :: PtrArr_DefineVersion
   PUBLIC :: PtrArr_ErrMsg
@@ -81,6 +83,9 @@ CONTAINS
 !################################################################################
 !################################################################################
 
+  ! Elemental function to test the status of the allocatable components
+  ! of a PtrArr object.
+  
   ELEMENTAL FUNCTION PtrArr_Associated(self) RESULT(alloc_stat)
     TYPE(PtrArr_type), INTENT(IN) :: self
     LOGICAL :: alloc_stat
@@ -89,6 +94,8 @@ CONTAINS
 
 
 
+  ! Elemental subroutine to re-initialize PtrArr objects.
+  
   ELEMENTAL SUBROUTINE PtrArr_Destroy(self)
     TYPE(PtrArr_type), INTENT(OUT) :: self
     self%Is_Allocated = .FALSE.
@@ -96,6 +103,8 @@ CONTAINS
 
 
 
+  ! Elemental subroutine to create an instance of a PtrArr object.
+  
   ELEMENTAL SUBROUTINE PtrArr_Create(self, n)
     ! Arguments
     TYPE(PtrArr_type), INTENT(OUT) :: self
@@ -120,6 +129,8 @@ CONTAINS
 
 
 
+  ! Function to set the value of PtrArr object components.
+  
   FUNCTION PtrArr_SetValue(self,Name,Arr) RESULT(err_stat)
     ! Arguments
     TYPE(PtrArr_type),      INTENT(IN OUT) :: self
@@ -154,6 +165,8 @@ CONTAINS
   END FUNCTION PtrArr_SetValue
   
   
+  
+  ! Function to get the value of PtrArr object components.
   
   FUNCTION PtrArr_GetValue(self,n_Points,Name,Arr) RESULT(err_stat)
     ! Arguments
@@ -194,6 +207,117 @@ CONTAINS
   
   
   
+  ! Function to convert a vector of data into a PtrArr object array.
+  
+  FUNCTION PtrArr_FromVector(self,n_Points,Vector) RESULT(err_stat)
+    ! Arguments
+    TYPE(PtrArr_type), INTENT(OUT) :: self(:)
+    INTEGER          , INTENT(IN)  :: n_Points(:)
+    REAL(fp)         , INTENT(IN)  :: Vector(:)
+    ! Function result
+    INTEGER :: err_stat
+    ! Local parameters
+    CHARACTER(*), PARAMETER :: ROUTINE_NAME = 'PtrArr_FromVector'
+    ! Local variables
+    CHARACTER(ML) :: msg
+    INTEGER :: n_elements
+    INTEGER :: n, i1, i2
+        
+    ! Set up
+    err_stat = SUCCESS
+    n_elements = SIZE(self)
+    ! ...Check inputs
+    IF ( SIZE(n_Points) /= n_elements ) THEN
+      err_stat = FAILURE
+      msg = 'Size of output object and input n_Points arrays are different'
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat)
+      RETURN
+    END IF
+    IF ( SUM(n_Points) /= SIZE(Vector) ) THEN
+      err_stat = FAILURE
+      msg = 'Sum of input n_Points and size of input Vector are different'
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat)
+      RETURN
+    END IF
+    
+    
+    ! Transfer data from vector to object array
+    i1 = 1
+    DO n = 1, n_elements
+      i2 = i1 + n_Points(n) - 1
+      err_stat = PtrArr_SetValue( self(n), Arr=Vector(i1:i2) )
+      IF ( err_stat /= SUCCESS ) THEN
+        WRITE( msg,'("Error setting value for object #",i0)' ) n
+        CALL Display_Message(ROUTINE_NAME, msg, err_stat)
+        RETURN
+      END IF
+      i1 = i2 + 1
+    END DO
+    
+  END FUNCTION PtrArr_FromVector
+  
+  
+  
+  ! Function to convert a PtrArr object array into a vector of data.
+  
+  FUNCTION PtrArr_ToVector(self,Vector) RESULT(err_stat)
+    ! Arguments
+    TYPE(PtrArr_type)    , INTENT(IN)  :: self(:)
+    REAL(fp), ALLOCATABLE, INTENT(OUT) :: Vector(:)
+    ! Function result
+    INTEGER :: err_stat
+    ! Local parameters
+    CHARACTER(*), PARAMETER :: ROUTINE_NAME = 'PtrArr_ToVector'
+    ! Local variables
+    CHARACTER(ML) :: msg
+    CHARACTER(ML) :: alloc_msg
+    INTEGER :: alloc_stat
+    INTEGER :: n_elements
+    INTEGER :: n, i1, i2
+    INTEGER :: n_points(SIZE(self))
+    REAL(fp), ALLOCATABLE :: arr(:)
+        
+    ! Set up
+    err_stat = SUCCESS
+    n_elements = SIZE(self)
+
+    ! Get point numbering and allocate
+    DO n = 1, n_elements    
+      err_stat = PtrArr_GetValue( self(n), n_Points = n_points(n) )
+      IF ( err_stat /= SUCCESS ) THEN
+        WRITE( msg,'("Error getting n_Points for object #",i0)' ) n
+        CALL Display_Message(ROUTINE_NAME, msg, err_stat)
+        RETURN
+      END IF
+    END DO
+    ALLOCATE(Vector(SUM(n_points)), STAT=alloc_stat, ERRMSG=alloc_msg)
+    IF ( alloc_stat /= 0 ) THEN
+      err_stat = FAILURE
+      msg = 'Error allocating output vector - '//TRIM(alloc_msg)
+      CALL Display_Message(ROUTINE_NAME, msg, err_stat)
+      RETURN
+    END IF
+
+    ! Transfer data from object array to vector
+    i1 = 1
+    DO n = 1, n_elements
+      i2 = i1 + n_points(n) - 1
+      err_stat = PtrArr_GetValue( self(n), Arr=arr )
+      IF ( err_stat /= SUCCESS ) THEN
+        WRITE( msg,'("Error getting value from object #",i0)' ) n
+        CALL Display_Message(ROUTINE_NAME, msg, err_stat)
+        RETURN
+      END IF
+      Vector(i1:i2) = arr
+      i1 = i2 + 1
+    END DO
+    
+  END FUNCTION PtrArr_ToVector
+  
+  
+  
+  ! Subroutine to view the contents of a PtrArr object.
+  
   SUBROUTINE PtrArr_Inspect(self)
     TYPE(PtrArr_type), INTENT(IN) :: self
     ! Output the PtrArr components     
@@ -208,6 +332,8 @@ CONTAINS
 
 
 
+  ! Subroutine to return the PtrArr definition module version information.
+  
   SUBROUTINE PtrArr_DefineVersion( Id )
     CHARACTER(*), INTENT(OUT) :: Id
     Id = MODULE_VERSION_ID
@@ -215,6 +341,8 @@ CONTAINS
 
 
 
+  ! Subroutine to display the PtrArr object error message (if any).
+  
   SUBROUTINE PtrArr_ErrMsg(self)
     TYPE(PtrArr_type), INTENT(IN) :: self
     CHARACTER(*), PARAMETER :: ROUTINE_NAME = 'PtrArr_ErrMsg'
@@ -231,6 +359,9 @@ CONTAINS
 !##################################################################################
 !##################################################################################
 
+  ! Elemental function to test the equality of two PtrArr objects.
+  ! Used in OPERATOR(==) interface block.
+  
   ELEMENTAL FUNCTION PtrArr_Equal( x, y ) RESULT( is_equal )
     TYPE(PtrArr_type), INTENT(IN) :: x, y
     LOGICAL :: is_equal
