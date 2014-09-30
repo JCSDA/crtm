de909cc6fc197a5e9e67d567c0c69b78e72d357e
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov@281fb692-3a18-0410-826d-9e847983f365>
2014-09-30 19:40:46 +0000

    src/Coefficients/CloudCoeff/CloudCoeff_Inspect
      * CloudCoeff_Inspect.f90: Updated to accept command line argument to pause
        the output of the data to screen.
    
      * Makefile: Updated to use Gnu double colon syntax. Removed unneeded targets.
    
    
    
    Former-commit-id: dc33bc2d34e330951729a323036c3cfeabdf319f

--- src/Coefficients/CloudCoeff/CloudCoeff_Inspect/CloudCoeff_Inspect.f90
+++ src/Coefficients/CloudCoeff/CloudCoeff_Inspect/CloudCoeff_Inspect.f90
@@ -23,6 +23,7 @@ PROGRAM CloudCoeff_Inspect
   ! Disable implicit typing
   IMPLICIT NONE
 
+
   ! ----------
   ! Parameters
   ! ----------
@@ -30,44 +31,78 @@ PROGRAM CloudCoeff_Inspect
   CHARACTER(*), PARAMETER :: PROGRAM_RCS_ID = &
   '$Id$'
 
+
+  ! -----------------------
+  ! Command line processing
+  ! -----------------------
+  ! Default definition
+  LOGICAL, PARAMETER :: DEFAULT_PAUSE = .FALSE.
+  ! Namelist definition of the command line arguments
+  LOGICAL :: pause = DEFAULT_PAUSE
+  NAMELIST /cmd/ pause
+  ! Variable definitions used in parsing command line
+  CHARACTER(2000) :: cmd_string='', arg_string=''
+  CHARACTER(256)  :: io_msg
+  INTEGER         :: io_stat
+  INTEGER         :: n, n_cmd_args
+
+
   ! ---------
   ! Variables
   ! ---------
-  INTEGER :: Error_Status
-  CHARACTER(256) :: Filename
-  TYPE(CloudCoeff_type) :: C
-
+  CHARACTER(256) :: msg
+  CHARACTER(256) :: filename
+  INTEGER :: err_stat
+  TYPE(CloudCoeff_type) :: coeffs
+  
+  
   ! Output program header
   CALL Program_Message( PROGRAM_NAME, &
                         'Program to display the contents of a CRTM Binary format '//&
                         'CloudCoeff file to stdout.', &
                         '$Revision$' )
 
+  
+  ! Get command line arguments as a string ready for namelist
+  n_cmd_args = COMMAND_ARGUMENT_COUNT()
+  IF ( n_cmd_args > 0 ) THEN
+    ! ...Extract individual arguments into string
+    DO n = 1, n_cmd_args
+      CALL GET_COMMAND_ARGUMENT(n, arg_string)
+      cmd_string = TRIM(cmd_string)//' '//TRIM(arg_string)
+    END DO
+    ! ...Add namelist prefix and terminator
+    cmd_string = '&cmd '//TRIM(cmd_string)//' /'
+    ! ...Internal read of namelist
+    READ(cmd_string, NML    = cmd    , &
+                     IOSTAT = io_stat, &
+                     IOMSG  = io_msg)
+    IF (io_stat /= 0) THEN
+      msg = 'Command line argument retrieval failed - '//TRIM(io_msg)
+      CALL Display_Message(PROGRAM_NAME, msg, FAILURE); STOP
+    END IF
+  END IF
+
+ 
   ! Get the filename
   WRITE( *,FMT='(/5x,"Enter the Binary CloudCoeff filename: ")',ADVANCE='NO' )
-  READ( *,'(a)' ) Filename
-  Filename = ADJUSTL( Filename )
-  IF ( .NOT. File_Exists( TRIM( Filename ) ) ) THEN
-    CALL Display_Message( PROGRAM_NAME, &
-                          'File '//TRIM( Filename )//' not found.', &
-                          FAILURE )
-    STOP
-  END IF
+  READ( *,'(a)' ) filename
+  filename = ADJUSTL(filename)
+
 
   ! Read the binary data file
-  Error_Status = CloudCoeff_Binary_ReadFile( Filename, C )
-  IF ( Error_Status /= SUCCESS ) THEN
-    CALL Display_Message( PROGRAM_NAME, &
-                          'Error reading Binary CloudCoeff file '//&
-                          TRIM(Filename), &
-                          Error_Status )
-    STOP
+  err_stat = CloudCoeff_Binary_ReadFile( filename, coeffs )
+  IF ( err_stat /= SUCCESS ) THEN
+    msg = 'Error reading Binary CloudCoeff file '//TRIM(filename)
+    CALL Display_Message( PROGRAM_NAME, msg, err_stat ); STOP
   END IF
 
+
   ! Display the contents
-  CALL Inspect( C )
+  CALL Inspect( coeffs, Pause=pause )
+
 
   ! Clean up
-  CALL CloudCoeff_Destroy( C )
+  CALL CloudCoeff_Destroy( coeffs )
 
 END PROGRAM CloudCoeff_Inspect
