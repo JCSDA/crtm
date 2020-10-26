785dca1c32fbb371d75634c0c98fe4c3e595b788
BenjaminTJohnson <Benjamin.T.Johnson@noaa.gov>
2020-10-26 18:29:03 +0000

    fix: updated Get_CRTM_Binary_Files.sh to point to larger fix directory binary file.

--- Get_CRTM_Binary_Files.sh
+++ Get_CRTM_Binary_Files.sh
@@ -1,14 +1,20 @@
-if test -f "fix_crtm-internal_develop_2.tgz"; then
-		if [ -d "fix/" ]; then #fix directory exists
-				echo "fix/ exists, doing nothing."
-		else
-				#untar the file and move directory to fix
-				tar -zxvf fix_crtm-internal_develop_2.tgz
-				mv fix_crtm-internal_develop fix
-		fi 
+filename="fix_crtm-internal_develop_2.tgz" #rel 2.3_jedi files
+
+if test -f "$filename"; then
+    if [ -d "fix/" ]; then #fix directory exists
+        echo "fix/ already exists, doing nothing."
+    else
+        #untar the file and move directory to fix
+				mkdir fix/
+        tar -zxvf $filename --directory fix/
+				echo "fix/ directory created from existing $filename file."
+    fi 
 else
     #download, untar, move
-		wget -q ftp://ftp.ucar.edu/pub/cpaess/bjohns/fix_crtm-internal_develop_2.tgz #full set of CRTM binary files
-		tar -zxvf fix_crtm-internal_develop_2.tgz
-		mv fix_crtm-internal_develop fix
+		echo "downloading $filename, please wait about 1-2 minutes."
+    wget -q ftp://ftp.ucar.edu/pub/cpaess/bjohns/$filename #jedi set of CRTM binary files
+		mkdir fix/
+    tar -zxvf $filename --directory fix/
+		echo "fix/ directory created from downloaded $filename."
 fi
+echo "Completed."
